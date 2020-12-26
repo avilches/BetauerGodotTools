0b4310ddbb9a57410b7f04ae864347617ea02f21
Alberto Vilches <vilches@gmail.com>
2020-12-26 19:46:35 +0100

    WorldComplete scene -> change level

--- Scenes/WorldComplete.gd
+++ Scenes/WorldComplete.gd
@@ -1,8 +1,13 @@
 extends Area2D
 
+
+export(String, FILE, "*.tscn") var next_scene;
+
 func _physics_process(delta):
 	var bodies = get_overlapping_bodies();
 	for body in bodies:
 		if body.name == "Player":
-			get_tree().change_scene("res://Worlds/World2.tscn")
+			# var scene_name = get_tree().get_current_scene().get_name()
+			get_tree().change_scene(next_scene)
+
 			
