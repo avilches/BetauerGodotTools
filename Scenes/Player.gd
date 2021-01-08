d160929c3a8edbdcbabff4d41c60be8be5937cf6
Alberto Vilches <vilches@gmail.com>
2021-01-08 11:25:34 +0100

    Better debugging: using delta instead OS time

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -6,15 +6,16 @@ extends KinematicBody2D
 
 const DEBUG_MAX_SPEED = false
 const DEBUG_MOTION = false
-const DEBUG_ACCELERATION = true
+const DEBUG_ACCELERATION = false
+const DEBUG_JUMP = false
 
 # ground
-const TIME_TO_MAX_SPEED = 0.2          # seconds to reach the max speed 0=immediate
+const TIME_TO_MAX_SPEED = 0.4          # seconds to reach the max speed 0=immediate
 const MAX_SPEED = 200                  # pixels/seconds
 const STOP_IF_SPEED_IS_LESS_THAN = 20 # pixels/seconds
 const FRICTION = 0.7                  # 0=stop 0.9=10%/frame 0.99=ice!!
 const COYOTE_TIME = 0.1               # seconds. How much time the player can jump when falling 
-const JUMP_HELPER_TIME = 90           # milliseconds. If the user press jump just before land
+const JUMP_HELPER_TIME = 0.12         # seconds. If the user press jump just before land
 onready var ACCELERATION = MAX_SPEED*1000 if TIME_TO_MAX_SPEED == 0 else MAX_SPEED/TIME_TO_MAX_SPEED
 const STOP_ON_SLOPES = true
 
@@ -30,7 +31,7 @@ const JUMP_HEIGHT = 60                # jump max pixels
 const MAX_JUMP_TIME = 0.4             # jump max time
 const MAX_FALLING_SPEED = 2000        # max speed in free fall
 const AIR_RESISTANCE = 0.6     
-const MAX_JUMPS = 2
+const MAX_JUMPS = 1
 onready var GRAVITY = (2 * JUMP_HEIGHT) / pow(MAX_JUMP_TIME, 2)
 onready var JUMP_FORCE = GRAVITY * MAX_JUMP_TIME
 onready var JUMP_FORCE_MIN = JUMP_FORCE / 2
@@ -50,16 +51,17 @@ onready var sprite = spriteIdle
 var motion = Vector2.ZERO
 var x_input:float = 0;
 var y_input:float = 0;
+
 # Debug the motion change
-var lastPos = 0
+var movStartPosMAXSPEED = 0
 var lastMotion = Vector2.ZERO
-var lastStart = Vector2.ZERO
-var movStartTime = 0
+var movStartTimeMAXSPEED = Vector2.ZERO
+var movStartTimeACC = -1
 
 var jumps = 0
 var canJump = false
 var isJumping = false
-var time_jump_pressed = 0
+var time_jump_pressed = -1
 
 func flip(left):
 	sprite.flip_h = !left;
@@ -73,7 +75,7 @@ func change_sprite(nextSprite):
 func _process(delta):
 	var x_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	lateral_movement(x_input, delta)
-	jump()
+	jump(delta)
 	
 	if x_input != 0:
 		flip(x_input < 0)
@@ -104,7 +106,6 @@ func _ready():
 	
 func lateral_movement(x_input, delta):
 	if x_input != 0:
-		debug_acceleration()
 		if sign(motion.x) != sign(x_input): # si cambia de direccion de repente...
 			# esto hace que se puede cambiar de direccion sin esperar a que decelere
 			motion.x = x_input * ACCELERATION * delta
@@ -115,56 +116,60 @@ func lateral_movement(x_input, delta):
 		if abs(motion.x) < STOP_IF_SPEED_IS_LESS_THAN: motion.x = 0
 		else: motion.x *= FRICTION if is_on_floor() else AIR_RESISTANCE
 
-func jump():
+func jump(delta):
 	if is_on_floor():
 		jumps = 0
 		canJump = true
 		
 	if Input.is_action_just_pressed("ui_up"):
-		time_jump_pressed = OS.get_ticks_msec()
+		time_jump_pressed = 0
+	elif time_jump_pressed != -1:
+		time_jump_pressed += delta
 		
 	if canJump:
-#		modulate = Color.white
-		var now = OS.get_ticks_msec()
-		if time_jump_pressed + JUMP_HELPER_TIME >= now:
-			time_jump_pressed = 0
+		if time_jump_pressed != -1 && time_jump_pressed <= JUMP_HELPER_TIME:
+			if DEBUG_JUMP:
+				if (time_jump_pressed > 0): print("Jump helper: ", time_jump_pressed, "s (Config:", JUMP_HELPER_TIME,")")
+				print("Jump start:", -JUMP_FORCE)
+			time_jump_pressed = -1
 			if SQUEEZE_JUMP_TIME != 0: sprite.scale = SQUEEZE_JUMP
-			print("Jump:",JUMP_FORCE)
+			
 			motion.y = -JUMP_FORCE
 			jumps = jumps + 1
 			isJumping = true
 			canJump = jumps < MAX_JUMPS
-
-	# jumping or falling
-#		modulate = Color.red
+			# TODO: controlar que el segundo salto se haga justo arriba, no al llegar al suelo
+			#       para que no coincida con el helper...
+	
+	
 	if Input.is_action_just_released("ui_up") and motion.y < -JUMP_FORCE_MIN and isJumping:
-		print("Jump from ",motion.y, " to ", JUMP_FORCE_MIN)
+		if DEBUG_JUMP:
+			print("Short jump: deccelerating from ", motion.y, " to ", -JUMP_FORCE_MIN)
 		motion.y = -JUMP_FORCE_MIN
 
-func debug_acceleration():
+func debug_motion(delta):
 	if DEBUG_ACCELERATION && motion.x != 0:
 		if lastMotion.x == 0:
-			movStartTime = OS.get_ticks_msec() # starts to move
-		elif abs(motion.x) >= MAX_SPEED:
-			if movStartTime != 0:
-				print("FULL THROTLE IN "+str(OS.get_ticks_msec() - movStartTime))
-			movStartTime = 0
+			movStartTimeACC = 0 # starts to move
+		elif movStartTimeACC != -1:
+			movStartTimeACC += delta
+			if abs(motion.x) >= MAX_SPEED:
+				print("Full throtle ", motion.x, " in ", movStartTimeACC, "s")
+				movStartTimeACC = -1
 
-func debug_motion():
 	if DEBUG_MAX_SPEED:
 		if motion.x != 0:
 			if lastMotion.x == 0:
-				lastStart = OS.get_ticks_msec()
-				lastPos = get_position().x
+				movStartTimeMAXSPEED = 0
+				movStartPosMAXSPEED = get_position().x
 			else:
-				var elapsed = OS.get_ticks_msec() - lastStart
-				if elapsed > 1000 && lastPos != 0:
-					var distance = str(get_position().x - lastPos)
-					print("Moved from "+str(get_position().x)+" to "+str(lastPos)+": "+str(distance)+"/second")
-					lastStart = OS.get_ticks_msec()
-					lastPos = get_position().x
+				movStartTimeMAXSPEED += delta
+				if movStartTimeMAXSPEED >= 1 && movStartPosMAXSPEED != -1:
+					var distance = get_position().x - movStartPosMAXSPEED
+					print("Moved from ", movStartPosMAXSPEED, " to ",  get_position().x, " in ", movStartTimeMAXSPEED, "s. Speed: ", abs(round(distance)),"px/second")
+					movStartTimeMAXSPEED = -1
 		else:
-			lastPos = 0
+			movStartPosMAXSPEED = 0
 
 	if DEBUG_MOTION && (lastMotion.x != motion.x || lastMotion.y != motion.y): print(motion)
 
@@ -197,14 +202,14 @@ func _physics_process(delta):
 	motion.x = clamp(motion.x, -MAX_SPEED, MAX_SPEED)
 	motion.y = min(motion.y, MAX_FALLING_SPEED) # avoid gravity continue forever in free fall
 
-	debug_motion()
+	debug_motion(delta)
 
 	if PlatformManager.is_falling_from_platform(self) || isJumping:
 		var remain = move_and_slide(motion, Vector2.UP, true)
 		motion.y = remain.y # this line stops the gravity accumulation
 	else:
 		var remain = move_and_slide_with_snap(motion, SLOPE_RAYCAST_VECTOR, FLOOR, STOP_ON_SLOPES)
-		motion.y = remain.y # this line stops the gravity accumulation
+		motion.y = remain.y  # this line stops the gravity accumulation
 		#motion.x = remain.x  # this line should be always commented, player can't climb slopes with it!!
 		
 	lastMotion = motion
