b20227b38cd23d4a1794deddaac790160133593e
Alberto Vilches <vilches@gmail.com>
2021-01-10 19:10:28 +0100

    PlatformFollowPath speed is configurable

--- tools/platforms/PlatformFollowPath.gd
+++ tools/platforms/PlatformFollowPath.gd
@@ -2,17 +2,13 @@ class_name PlatformFollowPath
 extends MovingPlatform
 
 onready var path = $'Platform path/PathFollow2D'
-
 onready var original = position
-
-func _ready():
-	original = position
+export var move_speed:float = 10
 
 func _enter_tree():
 	original = position
 		
-var move_speed = 10
 func _physics_process(delta):
 	if (mov_enabled):
 		position = original + path.position
-		path.set_offset(path.get_offset() + 10 * delta)
+		path.set_offset(path.get_offset() + move_speed * delta)
