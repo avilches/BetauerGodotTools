cc4561b1ef7b1ceef33da80c811f74aefd6c1ad7
Alberto Vilches <vilches@gmail.com>
2021-02-03 00:44:40 +0100

    Add Vertical motion to player + DPad (button) support in DirectionInput

--- Characters/Player/States/PlayerState.cs
+++ Characters/Player/States/PlayerState.cs
@@ -1,55 +1,1 @@
-using Betauer.Characters.Player;
-using Betauer.Characters.Player.States;
-using Betauer.Tools.Input;
-using Betauer.Tools.Statemachine;
-using Godot;
-
-namespace Betauer.Characters.Player {
-    public abstract class PlayerState : State {
-
-        protected readonly PlayerController Player;
-
-        protected PlayerState(PlayerController player) {
-            Player = player;
-        }
-
-        public void Debug(bool flag, string message) {
-            if (flag) {
-                Debug(message);
-            }
-        }
-
-        public void Debug(string message) {
-            GD.Print("#" + Player.GetFrame() + ": " + GetType().Name + " | " + message);
-        }
-
-
-        protected float XInput => Player.PlayerActions.LateralMotion.Strength;
-        protected ActionState Jump => Player.PlayerActions.Jump;
-        protected ActionState Attack => Player.PlayerActions.Attack;
-        protected float Delta => Player.Delta;
-        protected Vector2 Motion => Player.Motion;
-        protected PlayerConfig PlayerConfig => Player.PlayerConfig;
-
-        protected void GoToRunState() {
-            // Change to run is immediate
-            Player.SetNextState(typeof(GroundStateRun), true);
-        }
-
-        protected void GoToIdleState() {
-            // Idle is deferred to the next frame
-            Player.SetNextState(typeof(GroundStateIdle));
-        }
-
-        protected void GoToFallState() {
-            // fall next frame
-            Player.SetNextState(typeof(AirStateFall));
-        }
-
-        protected void GoToJumpState() {
-            // Jump is immediate
-            Player.SetNextState(typeof(AirStateJump), true);
-        }
-
-    }
-}
+using Betauer.Characters.Player;using Betauer.Characters.Player.States;using Betauer.Tools.Input;using Betauer.Tools.Statemachine;using Godot;namespace Betauer.Characters.Player {    public abstract class PlayerState : State {        protected readonly PlayerController Player;        protected PlayerState(PlayerController player) {            Player = player;        }        public void Debug(bool flag, string message) {            if (flag) {                Debug(message);            }        }        public void Debug(string message) {            GD.Print("#" + Player.GetFrame() + ": " + GetType().Name + " | " + message);        }        protected float XInput => Player.PlayerActions.LateralMotion.Strength;        protected bool IsRight => XInput > 0;        protected bool IsLeft => XInput < 0;        protected float YInput => Player.PlayerActions.VerticalMotion.Strength;        protected bool IsUp => YInput < 0;        protected bool IsDown => YInput > 0;        protected ActionState Jump => Player.PlayerActions.Jump;        protected ActionState Attack => Player.PlayerActions.Attack;        protected float Delta => Player.Delta;        protected Vector2 Motion => Player.Motion;        protected PlayerConfig PlayerConfig => Player.PlayerConfig;        protected void GoToRunState() {            // Change to run is immediate            Player.SetNextState(typeof(GroundStateRun), true);        }        protected void GoToIdleState() {            // Idle is deferred to the next frame            Player.SetNextState(typeof(GroundStateIdle));        }        protected void GoToFallState() {            // fall next frame            Player.SetNextState(typeof(AirStateFall));        }        protected void GoToJumpState() {            // Jump is immediate            Player.SetNextState(typeof(AirStateJump), true);        }    }}
--- Tools/Input/DirectionalInput.cs
+++ Tools/Input/DirectionalInput.cs
@@ -4,40 +4,65 @@ using Godot;
 namespace Betauer.Tools.Input {
     public class DirectionInput : IActionUpdate {
         public static readonly ISet<int> Empty = new HashSet<int>();
-        public static readonly ISet<int> CursorPositive = new HashSet<int>{ (int)KeyList.Right };
-        public static readonly ISet<int> CursorNegative = new HashSet<int>{ (int)KeyList.Left };
 
-        private ISet<int> _positives = CursorPositive;
-        private ISet<int> _negatives = CursorNegative;
-        private JoystickList _axis = JoystickList.Axis0;
+        public static readonly ISet<int> CursorLateralPositive = new HashSet<int>{ (int)KeyList.Right };
+        public static readonly ISet<int> CursorLateralNegative = new HashSet<int>{ (int)KeyList.Left };
+        public static readonly ISet<int> DpadLateralPositive = new HashSet<int>{ (int)JoystickList.DpadRight };
+        public static readonly ISet<int> DpadLateralNegative = new HashSet<int>{ (int)JoystickList.DpadLeft };
+
+        public static readonly ISet<int> CursorVerticalPositive = new HashSet<int>{ (int)KeyList.Down };
+        public static readonly ISet<int> CursorVerticalNegative = new HashSet<int>{ (int)KeyList.Up };
+        public static readonly ISet<int> DpadVerticalPositive = new HashSet<int>{ (int)JoystickList.DpadDown };
+        public static readonly ISet<int> DpadVerticalNegative = new HashSet<int>{ (int)JoystickList.DpadUp };
+
+        private ISet<int> _positiveKeys;
+        private ISet<int> _negativeKeys;
+        private ISet<int> _positiveButtons;
+        private ISet<int> _negativeButtons;
+        private JoystickList _axis;
+        
         private readonly PlayerActions _playerActions;
 
-        private float _inputPositive = 0f;
-        private float _inputNegative = 0f;
-        private float _input = 0f;
+        private float _inputPositiveKey = 0f;
+        private float _inputNegativeKey = 0f;
+        private float _inputPositiveButton = 0f;
+        private float _inputNegativeButton = 0f;
+        private float _inputTotalKey = 0f;
+        private float _inputTotalButton = 0f;
         private float _axisValue = 0f;
-        private readonly int _deviceId = -1;
+        public int DeviceId = -1;
+        private bool _buttons = false;
 
         public string Name;
         public float AxisDeadZone = 0.5f;
-        public float Strength => _playerActions.IsUsingKeyboard ? _input : _axisValue;
+        public float Strength => _playerActions.IsUsingKeyboard ? _inputTotalKey : _buttons?_inputTotalButton:_axisValue;
 
         public DirectionInput(string name, PlayerActions playerActions, int deviceId) {
             Name = name;
             _playerActions = playerActions;
-            _deviceId = deviceId;
+            DeviceId = deviceId;
+
+            ClearConfig();
         }
 
         public override bool Update(EventWrapper w) {
             if (!Enabled) return false;
-            if (LateralAxisMovement(w)) {
+            if (CheckAxis(w)) {
                 _playerActions.IsUsingKeyboard = false;
+                _buttons = false;
                 return true;
             }
 
-            if (LateralKeyMovement(w)) {
-                _playerActions.IsUsingKeyboard = w.IsAnyKey();
-                _input = _inputPositive - _inputNegative;
+            if (CheckButtons(w)) {
+                _playerActions.IsUsingKeyboard = false;
+                _buttons = true;
+                _inputTotalButton = _inputPositiveButton - _inputNegativeButton;
+                return true;
+            }
+
+            if (CheckKeys(w)) {
+                _playerActions.IsUsingKeyboard = true;
+                _inputTotalKey = _inputPositiveKey - _inputNegativeKey;
                 return true;
             }
 
@@ -47,20 +72,32 @@ namespace Betauer.Tools.Input {
         public override void ClearJustState() {
         }
 
-        private bool LateralKeyMovement(EventWrapper w) {
-            if (w.IsKey(_positives)) {
-                _inputPositive = w.Pressed ? w.GetStrength() : 0;
+        private bool CheckKeys(EventWrapper w) {
+            if (w.IsKey(_positiveKeys)) {
+                _inputPositiveKey = w.Pressed ? w.GetStrength() : 0;
                 return true;
-            } else if (w.IsKey(_negatives)) {
-                _inputNegative = w.Pressed ? w.GetStrength() : 0;
+            } else if (w.IsKey(_negativeKeys)) {
+                _inputNegativeKey = w.Pressed ? w.GetStrength() : 0;
                 return true;
             }
 
             return false;
         }
 
-        private bool LateralAxisMovement(EventWrapper w) {
-            if (w.IsAxis((int) _axis, _deviceId)) {
+        private bool CheckButtons(EventWrapper w) {
+            if (w.IsButton(_positiveButtons, DeviceId)) {
+                _inputPositiveButton = w.Pressed ? w.GetStrength() : 0;
+                return true;
+            } else if (w.IsButton(_negativeButtons, DeviceId)) {
+                _inputNegativeButton = w.Pressed ? w.GetStrength() : 0;
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool CheckAxis(EventWrapper w) {
+            if (w.IsAxis((int) _axis, DeviceId)) {
                 _axisValue = w.GetStrength(AxisDeadZone);
                 return true;
             }
@@ -68,29 +105,29 @@ namespace Betauer.Tools.Input {
             return false;
         }
 
-        public DirectionInput Configure(ISet<int> positives, ISet<int> negatives, JoystickList axis, float axisDeadZone) {
-            AxisDeadZone = axisDeadZone;
-            ConfigureKeys(positives, negatives);
-            ConfigureAxis(axis);
-            return this;
-        }
-
-        public DirectionInput ConfigureKeysOnly(ISet<int> positives, ISet<int> negatives) {
-            ConfigureKeys(positives, negatives);
+        public DirectionInput ClearConfig() {
+            _positiveKeys = Empty;
+            _negativeKeys = Empty;
+            _positiveButtons = Empty;
+            _negativeButtons = Empty;
             _axis = JoystickList.InvalidOption;
             return this;
         }
 
-        public DirectionInput ConfigureKeys(ISet<int> positives, ISet<int> negatives) {
-            _positives = positives;
-            _negatives = negatives;
+        public DirectionInput ChangeDevice(int deviceId) {
+            this.DeviceId = deviceId;
             return this;
         }
 
-        public DirectionInput ConfigureAxisOnly(JoystickList axis) {
-            _positives = Empty;
-            _negatives = Empty;
-            ConfigureAxis(axis);
+        public DirectionInput ConfigureKeys(ISet<int> positives, ISet<int> negatives) {
+            _positiveKeys = positives;
+            _negativeKeys = negatives;
+            return this;
+        }
+
+        public DirectionInput ConfigureButtons(ISet<int> positives, ISet<int> negatives) {
+            _positiveButtons = positives;
+            _negativeButtons = negatives;
             return this;
         }
 
