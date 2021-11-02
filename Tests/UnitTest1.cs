b0e5185dc2bafecb36f9b98ae5dba16c8d3331e1
Alberto Vilches <vilches@gmail.com>
2021-11-02 23:34:02 +0100

    Basic test (created with Rider)

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace Betauer.Tools.Input.X {
+    public class Tests {
+        [SetUp]
+        public void Setup() {
+        }
+
+        [Test]
+        public void Test1() {
+            Assert.Pass();
+        }
+    }
+}
