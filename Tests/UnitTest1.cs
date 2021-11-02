b945e4cc38db7b1a37d10a1da06172be6ead6216
Alberto Vilches <vilches@gmail.com>
2021-11-03 00:03:05 +0100

    Fix Godot usage from tests

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,7 +1,9 @@
+using Betauer.Tools.Events;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
+using Godot;
 
-namespace Betauer.Tools.Input.X {
+namespace Tests {
     public class Tests {
         [SetUp]
         public void Setup() {
@@ -9,6 +11,11 @@ namespace Betauer.Tools.Input.X {
 
         [Test]
         public void Test1() {
+            var a = TopicMap.Instance;
+            var godotUnicastTopic1 = new GodotUnicastTopic<BodyOnArea2D>();
+            var gototUnicastTopic2 = new GodotUnicastTopic<BodyOnArea2D>();
+            a.AddTopic("topic1", godotUnicastTopic1);
+            a.AddTopic("topic2", gototUnicastTopic2);
             Assert.Pass();
         }
     }
