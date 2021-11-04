493e7a95cdbf7ab014390e080076bdaa70977648
Alberto Vilches <vilches@gmail.com>
2021-11-04 22:27:28 +0100

    Debug always enabled in test

--- Tools/Events/Debug.cs
+++ Tools/Events/Debug.cs
@@ -2,17 +2,21 @@ using Godot;
 
 namespace Tools.Events {
     public class Debug {
-        public const bool DEBUG_STAGE = true;
-        public const bool DEBUG_REGISTER = false;
-        public const bool DEBUG_RESOLUTION = false;
-        public const bool DEBUG_EVENT_LISTENER = false;
+        public static bool TESTING = false;
+
+        private const bool _DEBUG_STAGE = false;
+        private const bool _DEBUG_REGISTER = false;
+        private const bool _DEBUG_RESOLUTION = false;
+        private const bool _DEBUG_EVENT_LISTENER = false;
+
+        public static bool DEBUG_EVENT_LISTENER => _DEBUG_EVENT_LISTENER || TESTING;
 
         public static void Register(string type, Node node) {
-            if (!DEBUG_REGISTER) return;
+            if (!TESTING && !_DEBUG_REGISTER) return;
             GD.Print("+"+ type+": \""+node.Name+ "\" ("+node.GetType()+") 0x"+node.GetHashCode().ToString("X"));
         }
         public static void Stage(string message) {
-            if (!DEBUG_STAGE) return;
+            if (!TESTING && !_DEBUG_STAGE) return;
             GD.Print("[Stage] " + message);
         }
     }
