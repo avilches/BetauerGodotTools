552c8c41e6534df60787e0d99b1328bf377b87b2
Alberto Vilches <194074+avilches@users.noreply.github.com>
2022-01-10 22:59:10 +0100

    PropertyRestorer.cs: add Save() to the restores, so they can be used multiple times

--- Betauer/Animation/PropertyRestorer.cs
+++ Betauer/Animation/PropertyRestorer.cs
@@ -2,36 +2,43 @@ using Godot;
 
 namespace Betauer.Animation {
     public interface IRestorer {
-        public void Rollback();
+        public void Restore();
     }
 
     public static class RestoreExtensions {
         public static IRestorer Save(this Node2D node) {
-            return new Node2DRestorer(node);
+            return new Node2DRestorer(node).Save();
         }
 
         public static IRestorer Save(this Control node) {
-            return new ControlRestorer(node);
+            return new ControlRestorer(node).Save();
         }
     }
 
     public class Node2DRestorer : IRestorer {
-        private readonly IRestorer? _pivotOffsetRestorer;
-        private readonly Color _modulate;
-        private readonly Color _selfModulate;
-        private readonly Transform2D _transform;
+        private IRestorer? _pivotOffsetRestorer;
+        private Color _modulate;
+        private Color _selfModulate;
+        private Transform2D _transform;
         private readonly Node2D _node2D;
+        private bool _saved = false;
 
         public Node2DRestorer(Node2D node2D) {
             _node2D = node2D;
-            _pivotOffsetRestorer = node2D is Sprite sprite ? new SpritePivotOffsetRestorer(sprite) : null;
-            _modulate = node2D.Modulate;
-            _selfModulate = node2D.SelfModulate;
-            _transform = node2D.Transform;
         }
 
-        public void Rollback() {
-            _pivotOffsetRestorer?.Rollback();
+        public Node2DRestorer Save() {
+            _pivotOffsetRestorer = _node2D is Sprite sprite ? new SpritePivotOffsetRestorer(sprite) : null;
+            _modulate = _node2D.Modulate;
+            _selfModulate = _node2D.SelfModulate;
+            _transform = _node2D.Transform;
+            _saved = true;
+            return this;
+        }
+
+        public void Restore() {
+            if (!_saved) return;
+            _pivotOffsetRestorer?.Restore();
             _node2D.Modulate = _modulate;
             _node2D.SelfModulate = _selfModulate;
             _node2D.Transform = _transform;
@@ -39,31 +46,38 @@ namespace Betauer.Animation {
     }
 
     public class ControlRestorer : IRestorer {
-        private readonly IRestorer _pivotOffsetRestorer;
-        private readonly Color _modulate;
-        private readonly Color _selfModulate;
-        private readonly Vector2 _position;
-        private readonly Vector2 _scale;
-        private readonly float _rotation;
+        private IRestorer _pivotOffsetRestorer;
+        private Color _modulate;
+        private Color _selfModulate;
+        private Vector2 _position;
+        private Vector2 _scale;
+        private float _rotation;
         private readonly Control _control;
+        private bool _saved = false;
 
         public ControlRestorer(Control control) {
             _control = control;
-            _pivotOffsetRestorer = new RectPivotOffsetRestorer(control);
-            _modulate = control.Modulate;
-            _selfModulate = control.SelfModulate;
-            _position = control.RectPosition;
-            _scale = control.RectScale;
-            _rotation = control.RectRotation;
         }
 
-        public void Rollback() {
+        public ControlRestorer Save() {
+            _pivotOffsetRestorer = new RectPivotOffsetRestorer(_control);
+            _modulate = _control.Modulate;
+            _selfModulate = _control.SelfModulate;
+            _position = _control.RectPosition;
+            _scale = _control.RectScale;
+            _rotation = _control.RectRotation;
+            _saved = true;
+            return this;
+        }
+
+        public void Restore() {
+            if (!_saved) return;
             _control.Modulate = _modulate;
             _control.SelfModulate = _selfModulate;
             _control.RectPosition = _position;
             _control.RectScale = _scale;
             _control.RectRotation = _rotation;
-            _pivotOffsetRestorer.Rollback();
+            _pivotOffsetRestorer.Restore();
         }
     }
 
@@ -77,7 +91,7 @@ namespace Betauer.Animation {
             _originalRectPivotOffset = node.RectPivotOffset;
         }
 
-        public void Rollback() {
+        public void Restore() {
             _node.RectPivotOffset = _originalRectPivotOffset;
         }
     }
@@ -93,7 +107,7 @@ namespace Betauer.Animation {
             _globalPosition = node.GlobalPosition;
         }
 
-        public void Rollback() {
+        public void Restore() {
             _node.Offset = _offset;
             _node.GlobalPosition = _globalPosition;
         }
@@ -105,7 +119,7 @@ namespace Betauer.Animation {
         private DummyRestorer() {
         }
 
-        public void Rollback() {
+        public void Restore() {
         }
     }
 }
