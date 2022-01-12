822a3e8aed28ea99ec2e12b5fa611e5095c2d23d
Alberto Vilches <194074+avilches@users.noreply.github.com>
2022-01-12 10:07:31 +0100

    MainMenu.cs: refactor builder + added Esc button to back action

--- Game/Controller/Menu/MainMenu.cs
+++ Game/Controller/Menu/MainMenu.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Betauer;
 using Betauer.Animation;
@@ -12,98 +9,117 @@ namespace Veronenger.Game.Controller.Menu {
     public class MainMenu : DiControl {
         [OnReady("MarginContainer/HBoxContainer/VBoxContainer/Menu")]
         private VBoxContainer _menuBase;
+        private MenuController _menuController;
 
-        private readonly Logger _logger = LoggerFactory.GetLogger(typeof(MainMenu));
         [Inject] public InputManager InputManager;
         [Inject] public GameManager GameManager;
 
-        private SceneTree _sceneTree;
-
-        private MultipleActionMenu _menu;
-
         private Launcher _launcher;
 
-
         public override async void Ready() {
-            Engine.TimeScale = 0.25f;
-            _sceneTree = GetTree();
             _launcher = new Launcher().CreateNewTween(this);
-            await BuildMenu();
+            _menuController = await BuildMenu();
             // Go(_mainMenu, _options, _optionsMenu);
         }
 
-        public async Task BuildMenu() {
+        public async Task<MenuController> BuildMenu() {
             foreach (var child in _menuBase.GetChildren()) (child as Node)?.Free();
 
-            _menu = new MultipleActionMenu(_menuBase);
-            BuildMainMenu();
-            BuildOptionsMenu();
-            await _menu.Show("Root");
-        }
-
-        private void BuildMainMenu() {
-            _menu.AddMenu("Root")
+            var mainMenu = new MenuController(_menuBase);
+            mainMenu.AddMenu("Root")
                 .AddButton("NewGame", "New game", async (ActionButton but) => {
                     GD.Print("New Game");
-                    var _continue = but.Menu.GetButton("Continue");
-                    _continue.Disabled = !_continue.Disabled;
+                    var continueButton = but.Menu.GetButton("Continue");
+                    continueButton!.Disabled = !continueButton.Disabled;
+                    continueButton.Restore();
                     await but.Refresh();
                 })
                 .AddButton("Continue", "Continue", async (ActionButton but) => {
                     GD.Print("Continue");
                     but.Disabled = true;
                     await but.Refresh();
+                    await _launcher.Play(Template.FadeOut, but, 0f,
+                            MenuEffectTime)
+                        .Await();
+
                 })
                 .AddButton("Options", "Options",
                     (ActionButton but) => { but.Go("Options", GoGoodbyeAnimation, GoNewMenuAnimation); })
-                .AddButton("Quit", "Quit", (ActionButton but) => { GameManager.Quit(); });
+                .AddButton("Quit", "Quit", (ActionButton but) => { GameManager.Quit(); })
+                .Save();
 
-            _menu.GetMenu("Root").GetButton("Continue").Disabled = true;
+            mainMenu.GetMenu("Root").GetButton("Continue")!.Disabled = true;
+
+            mainMenu.AddMenu("Options")
+                .AddButton("Video", "Video", async (ActionButton but) => { GD.Print("New Game"); })
+                .AddButton("Controls", "Controls", (ActionButton but) => GD.Print("Controls"))
+                .AddButton("Sound", "Sound", (ActionButton but) => GD.Print("Options"))
+                .AddButton("Back", "Back", async (ActionButton but) =>
+                    await but.Back(BackGoodbyeAnimation, BackNewMenuAnimation)
+                )
+                .Save();
+
+            await mainMenu.Show("Root");
+            return mainMenu;
         }
 
         private async Task GoGoodbyeAnimation(MenuTransition transition) {
-            // await _launcher.Play(Template.BackOutLeftFactory.Get(150), _menuHolder, 0f, MenuEffectTime).Await();
+            // await _launcher.Play(Template.BackOutLeftFactory.Get(150), transition.FromMenu.Control, 0f, MenuEffectTime).Await();
             GD.Print("Go1");
             await _launcher.Play(Template.RollOut, transition.FromButton, 0f, 0.25f).Await();
             GD.Print("Go2");
-            await _launcher.Play(Template.FadeOutDown, transition.FromMenu.Holder, 0f, 0.25f).Await();
+            await _launcher.Play(Template.FadeOutDown, transition.FromMenu.Control, 0f, 0.25f).Await();
         }
 
         private async Task GoNewMenuAnimation(MenuTransition transition) {
             int x = 0;
+
+            var s = SequenceBuilder.Create()
+                .AnimateSteps(null, Property.Modulate)
+                .From(new Color(1f,0f,0f)).To(new Color(1f,1f,1f,1f), 0.25f)
+                .EndAnimate()
+                .Parallel()
+                .ImportTemplate(Template.RotateInDownRight);
+
             GD.Print("Go3");
+            LoopStatus lastToWaitFor = null;
             foreach (var button in transition.ToMenu.GetButtons()) {
-                _launcher.Play(Template.RotateInDownRight, button as Control, x * 0.05f, MenuEffectTime)
-                    .Await();
+                var actionButton = button as ActionButton;
+                // actionButton.Modulate =
+                    // new Color(actionButton.Modulate.r, actionButton.Modulate.g, actionButton.Modulate.b, 0);
+                lastToWaitFor = _launcher.Play(s, actionButton, x * 0.05f, MenuEffectTime);
                 x++;
             }
+            await lastToWaitFor.Await();
             // await _launcher.Play(Template.BackInRightFactory.Get(200), _menuHolder, 0f, MenuEffectTime).Await();
         }
 
 
-        private void BuildOptionsMenu() {
-            _menu.AddMenu("Options")
-                .AddButton("Video","Video", async (ActionButton but) => { GD.Print("New Game"); })
-                .AddButton("Controls","Controls", (ActionButton but) => GD.Print("Controls"))
-                .AddButton("Sound","Sound", (ActionButton but) => GD.Print("Options"))
-                .AddButton("Back","Back", async (ActionButton but) =>
-                    but.Back(BackGoodbyeAnimation, BackNewMenuAnimation)
-                );
-        }
-
         private async Task BackGoodbyeAnimation(MenuTransition transition) {
-            await _launcher.Play(Template.BackOutRightFactory.Get(200), transition.FromMenu.Holder, 0f,
+            await _launcher.Play(Template.BackOutRightFactory.Get(200), transition.FromMenu.Control, 0f,
                     MenuEffectTime)
                 .Await();
         }
 
         private async Task BackNewMenuAnimation(MenuTransition transition) {
-            await _launcher.Play(Template.BackInLeftFactory.Get(150), transition.ToMenu.Holder, 0f,
+            await _launcher.Play(Template.BackInLeftFactory.Get(150), transition.ToMenu.Control, 0f,
                     MenuEffectTime)
                 .Await();
         }
 
         private const float MenuEffectTime = 0.25f;
+
+        public override void _Input(InputEvent @event) {
+            if (@event.IsAction("ui_cancel")) {
+                _menuController.Back();
+            }
+            var action = InputManager.OnEvent(@event);
+            if (action != null) {
+                // _sceneTree.SetInputAsHandled();
+                // InputMap.Singleton
+            }
+            InputManager.Debug(action);
+        }
 
 
         // private float delay = 0f;
