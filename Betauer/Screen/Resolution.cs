c2247f86a712094bc3eb956afa3e6d05a191669d
Alberto Vilches <194074+avilches@users.noreply.github.com>
2022-01-17 15:29:57 +0100

    Resolution.cs: added AspectRatio and Resolution list

--- Betauer/Screen/Resolution.cs
+++ Betauer/Screen/Resolution.cs
@@ -1,72 +1,171 @@
+using System;
+using System.Collections.Generic;
 using Godot;
 
 namespace Betauer.Screen {
-    public static class Resolution {
-        // 16:9 https://en.wikipedia.org/wiki/16:9_aspect_ratio
-        public static Vector2 FULLHD_DIV6 = new Vector2(320, 180); // 1920x1080 / 6
-        public static Vector2 FULLHD_DIV4 = new Vector2(480, 270); // 1920x1080 / 4
-        public static Vector2 FULLHD_DIV2 = new Vector2(960, 540); // 1920x1080 / 2
-        public static Vector2 FULLHD = new Vector2(1920, 1080);
-        public static Vector2 FULLHD_2K = new Vector2(2560, 1440); // 1920x1080 * 1.33 aka "2K"
-        public static Vector2 FULLHD_4K = new Vector2(3840, 2160); // 1920x1080 * 2 // aka 4K
+    public static class AspectRatios {
+        public static List<AspectRatio> All = new List<AspectRatio>();
+        public static AspectRatio Ratio5_4 = AddToAll(new AspectRatio(5, 4)); // 1.125
+        public static AspectRatio Ratio4_3 = AddToAll(new AspectRatio(4, 3)); // 1.3333333
+        public static AspectRatio Ratio5_3 = AddToAll(new AspectRatio(5, 3)); // 1.6666666
 
-        public static Vector2 FULLHD_DIV1_875 = new Vector2(1024, 576); // 1920x1080 / 1.875
-        public static Vector2 FULLHD_DIV1_5 = new Vector2(1280, 720); // 1920x1080 / 1.5
-        public static Vector2 FULLHD_DIV1_2 = new Vector2(1600, 900); // 1920x1080 / 1.2
-        public static Vector2 FULLHD_DIV3 = new Vector2(640, 360); // 1920x1080 / 3
+        public static AspectRatio Ratio3_2 = AddToAll(new AspectRatio(3, 2));
+
+        public static AspectRatio Ratio12_5 = AddToAll(new AspectRatio(12, 5));
+        // 2.4 aprox -> 3440/1440 = 2,38888889 = 0,011 threshold
+
+        // 1.5 aprox -> 2560/1700 = 1,50588235 = 0,00588235 threshold
+        public static AspectRatio Ratio16_10 = AddToAll(new AspectRatio(16, 10)); // 1.6
+
+        public static AspectRatio Ratio16_9 = AddToAll(new AspectRatio(16, 9));
+        // 1.7777 aprox -> 1366/768 = 1,77864583 = 0,00086813 threshold
+
+        public static AspectRatio Ratio17_9 = AddToAll(new AspectRatio(17, 9));
+        // 1.8888 aprox -> 4096/2160 = 1,8962963 = 0,0074 threshold
+
+        public static AspectRatio Ratio21_9 = AddToAll(new AspectRatio(21, 9));
+        // 2.3333 aprox -> 2560/1080 = 2,37037037 = 0,03703704 threshold
+
+        public static AspectRatio Ratio32_9 = AddToAll(new AspectRatio(32, 9)); // 3.5555
+        public static AspectRatio Ratio1_1 = AddToAll(new AspectRatio(1, 1)); // 1
+        public static AspectRatio Ratio4_1 = AddToAll(new AspectRatio(4, 1)); // 4
+
+        private static AspectRatio AddToAll(AspectRatio ratio) {
+            All.Add(ratio);
+            return ratio;
+        }
+
+        public static AspectRatio Get(Resolution resolution) => Get(resolution.Size);
+        public static AspectRatio Get(Vector2 resolution) => Get(resolution.x / resolution.y);
+        public static AspectRatio Get(int x, int y) => Get(x / (float)y);
+        public static AspectRatio Get(float ratio) => All.Find(aspect => aspect.Matches(ratio));
+    }
+
+    public static class Resolutions {
+        private static readonly List<Resolution> _all = new List<Resolution>();
+
+
+        // 16:9 https://en.wikipedia.org/wiki/16:9_aspect_ratio
+        public static Resolution FULLHD_DIV6 = AddToAll(new Resolution(320, 180)); // 1920x1080 / 6
+        public static Resolution FULLHD_DIV4 = AddToAll(new Resolution(480, 270)); // 1920x1080 / 4
+        public static Resolution FULLHD_DIV2 = AddToAll(new Resolution(960, 540)); // 1920x1080 / 2
+        public static Resolution FULLHD = AddToAll(new Resolution(1920, 1080));
+        public static Resolution FULLHD_2K = AddToAll(new Resolution(2560, 1440)); // 1920x1080 * 1.33 aka "2K"
+        public static Resolution FULLHD_4K = AddToAll(new Resolution(3840, 2160)); // 1920x1080 * 2 // aka 4K
+
+        public static Resolution FULLHD_DIV1_875 = AddToAll(new Resolution(1024, 576)); // 1920x1080 / 1.875
+        public static Resolution FULLHD_DIV1_5 = AddToAll(new Resolution(1280, 720)); // 1920x1080 / 1.5
+        public static Resolution FULLHD_DIV1_2 = AddToAll(new Resolution(1600, 900)); // 1920x1080 / 1.2
+        public static Resolution FULLHD_DIV3 = AddToAll(new Resolution(640, 360)); // 1920x1080 / 3
 
         // 16:10 https://en.wikipedia.org/wiki/16:10_aspect_ratio
-        public static Vector2 R1610SMALL1 = new Vector2(640, 400);
-        public static Vector2 R1610SMALL2 = new Vector2(960, 600);
-        public static Vector2 WXGA = new Vector2(1280, 800);
-        public static Vector2 WXGAplus = new Vector2(1440, 900);
-        public static Vector2 WSXGAplus = new Vector2(1680, 1050);
-        public static Vector2 WUXGA = new Vector2(1920, 1200);
-        public static Vector2 WQXGA = new Vector2(2560, 1600);
-        public static Vector2 WQUXGA = new Vector2(3840, 2400);
+        public static Resolution R1610SMALL1 = AddToAll(new Resolution(640, 400));
+        public static Resolution R1610SMALL2 = AddToAll(new Resolution(960, 600));
+        public static Resolution WXGA = AddToAll(new Resolution(1280, 800));
+        public static Resolution WXGAplus = AddToAll(new Resolution(1440, 900));
+        public static Resolution WSXGAplus = AddToAll(new Resolution(1680, 1050));
+        public static Resolution WUXGA = AddToAll(new Resolution(1920, 1200));
+        public static Resolution WQXGA = AddToAll(new Resolution(2560, 1600));
+        public static Resolution WQUXGA = AddToAll(new Resolution(3840, 2400));
 
         // 21:9 https://en.wikipedia.org/wiki/21:9_aspect_ratio
         // 64:27	2.370
-        public static Vector2 UWDIE237_0 = new Vector2(2560, 1080);
+        public static Resolution UWDIE237_0 = AddToAll(new Resolution(2560, 1080));
 
-        // public static Vector2 UWDIE237_1 = new Vector2(5120, 2160);
-        // public static Vector2 UWDIE237_2 = new Vector2(7680, 3240);
-        // public static Vector2 UWDIE237_3 = new Vector2(10240, 4320);
+        // public static Resolution UWDIE237_1 = AddToAll(new Resolution(5120, 2160));
+        // public static Resolution UWDIE237_2 = AddToAll(new Resolution(7680, 3240));
+        // public static Resolution UWDIE237_3 = AddToAll(new Resolution(10240, 4320));
         // 43:18	2.38
-        public static Vector2 UWDI238_0 = new Vector2(3440, 1440);
+        public static Resolution UWDI238_0 = AddToAll(new Resolution(3440, 1440));
 
-        // public static Vector2 UWDI238_1 = new Vector2(5160, 2160);
-        // public static Vector2 UWDI238_2 = new Vector2(6880, 2880);
+        // public static Resolution UWDI238_1 = AddToAll(new Resolution(5160, 2160));
+        // public static Resolution UWDI238_2 = AddToAll(new Resolution(6880, 2880));
         //	12:5	2.4
-        public static Vector2 UWDIE24_0 = new Vector2(1920, 800);
-        public static Vector2 UWDIE24_1 = new Vector2(2880, 1200);
-        public static Vector2 UWDIE24_2 = new Vector2(3840, 1600);
+        public static Resolution UWDIE24_0 = AddToAll(new Resolution(1920, 800));
+        public static Resolution UWDIE24_1 = AddToAll(new Resolution(2880, 1200));
+        public static Resolution UWDIE24_2 = AddToAll(new Resolution(3840, 1600));
 
-        public static Vector2 UWDIE24_3 = new Vector2(4320, 1800);
-        // public static Vector2 UWDIE24_4 = new Vector2(5760, 2400);
-        // public static Vector2 UWDIE24_5 = new Vector2(7680, 3200);
-        // public static Vector2 UWDIE24_6 = new Vector2(8640, 3600);
+        public static Resolution UWDIE24_3 = AddToAll(new Resolution(4320, 1800));
+
+        // public static Resolution UWDIE24_4 = AddToAll(new Resolution(5760, 2400));
+        // public static Resolution UWDIE24_5 = AddToAll(new Resolution(7680, 3200));
+        // public static Resolution UWDIE24_6 = AddToAll(new Resolution(8640, 3600));
+
+        private static Resolution AddToAll(Resolution resolution) {
+            _all.Add(resolution);
+            _all.Sort((left, right) => left.x * left.y > right.x * right.y ? 1 : -1);
+            return resolution;
+        }
+
+        internal static IList<Resolution> All() => new List<Resolution>(_all);
     }
 
-    public class ScreenConfiguration {
-        public Vector2 BaseResolution { get; }
+    public class AspectRatio {
+        internal const float Tolerance = 0.05f;
 
-        // disabled: while the framebuffer will be resized to match the game window, nothing will be upscaled or downscaled (this includes GUIs).
-        // 2d: the framebuffer is still resized, but GUIs can be upscaled or downscaled. This can result in blurry or pixelated fonts.
-        // viewport: the framebuffer is resized, but computed at the original size of the project. The whole rendering will be pixelated. You generally do not want this, unless it's part of the game style.
-        public SceneTree.StretchMode StretchMode { get; }
-        public SceneTree.StretchAspect StretchAspect { get; }
-        public int StretchShrink { get; }
+        public readonly string Name;
+        public readonly float Ratio;
 
+        public AspectRatio(int width, int height) {
+            Name = width + ":" + height;
+            Ratio = width / (float)height;
+            AspectRatios.All.Add(this);
+        }
 
-        public ScreenConfiguration(Vector2 baseResolution, SceneTree.StretchMode stretchMode,
-            SceneTree.StretchAspect stretchAspect, int stretchShrink = 1) {
-            BaseResolution = baseResolution;
-            StretchMode = stretchMode;
-            StretchAspect = stretchAspect;
-            StretchShrink = stretchShrink;
+        public bool Matches(Resolution resolution) => Matches(resolution.Size);
+        public bool Matches(Vector2 resolution) => Matches(resolution.x / resolution.y);
+        public bool Matches(int x, int y) => Matches(x / (float)y);
+        public bool Matches(float ratio) => Math.Abs(ratio - Ratio) < Tolerance;
+    }
+
+    public class Resolution {
+        /**
+         * Returns how many times can be multiplied the baseResolution without create a resolution bigger than maxSize
+         */
+        public static int CalculateMaxScale(Vector2 maxSize, Vector2 @base) {
+            return (int)Mathf.Max(
+                Mathf.Floor(Mathf.Min(maxSize.x / @base.x, maxSize.y / @base.y)), 1);
+        }
+
+        public readonly Vector2 Size;
+        public readonly AspectRatio? AspectRatio;
+
+        public Resolution(Vector2 size) {
+            Size = size;
+            AspectRatio = AspectRatios.Get(this);
+        }
+
+        public Resolution(int x, int y) : this(new Vector2(x, y)) {
+        }
+
+        public int x => (int)Size.x;
+        public int y => (int)Size.y;
+
+        public override string ToString() {
+            return AspectRatio != null ? $"{AspectRatio.Name} {x}x{y}" : $"{x}x{y}";
         }
     }
 
+    public class ScaledResolution : Resolution {
+        public readonly Vector2 Base;
+        public readonly Vector2 Scale;
 
+        public ScaledResolution(Vector2 @base, Vector2 size) : base(size) {
+            Base = @base;
+            Scale = size / @base;
+        }
+
+        public ScaledResolution(Vector2 @base, int x, int y) : this(@base, new Vector2(x, y)) {
+        }
+
+        public bool HasSameAspectRatio() => Math.Abs(Scale.x - Scale.y) < 0.00001f;
+
+        public bool IsPixelPerfectScale() => HasSameAspectRatio() && IsInteger(Scale.x);
+
+        private static bool IsInteger(float x) =>  Math.Abs(x - Math.Floor(x)) < 0.00001f;
+
+        // Please check IsPixelPerfectScale before to use this value!!
+        public int GetPixelPerfectScale() => (int)Math.Floor(Scale.x);
+
+    }
 }
