bb8596af14191bcd0b5f8f7083ced8d2594653b0
Alberto Vilches <194074+avilches@users.noreply.github.com>
2022-06-14 15:07:17 +0200

    New Betauer.Generator isolated project to generate GodotAction classes and signal constants/extension class

---
+++ Betauer.Generator/GeneratorScript.cs
@@ -0,0 +1,266 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Godot;
+using File = System.IO.File;
+
+/**
+     * Based on: https://github.com/semickolon/GodotRx/blob/6bf1a1ba9ffbe939e888d8d545c8d448a1f07bce/addons/godotrx/codegen.js
+     * 
+     * call with -s GeneratorScript.cs --no-window
+     */
+public class GeneratorScript : SceneTree {
+
+    private const string SignalExtensionsFile = "../Betauer.Core/SignalExtensions.cs";
+    private const string SignalConstantsFile = "../Betauer.Core/SignalConstants.cs";
+    private const string ActionClassesPath = "../Betauer.GodotAction/GodotAction";
+    
+    private const string ActionClassNamespace = "Betauer.GodotAction";
+    
+    public override void _Initialize() {
+        while (Root.GetChildCount() > 0) Root.RemoveChild(Root.GetChild(0));
+        var classes = LoadGodotClasses();
+        WriteAllActionClasses(classes);
+        WriteSignalExtensionsClass(classes);
+        WriteSignalConstantsClass(classes);
+        Quit(0);
+    }
+
+    private void WriteSignalExtensionsClass(List<GodotClass> classes) {
+        List<string> allMethods = classes
+            .Where(godotClass => godotClass.Signals.Count > 0 &&
+                                 !godotClass.IsEditor)
+            .SelectMany(godotClass => godotClass.Signals)
+            .Select(CreateSignalExtensionMethod)
+            .ToList();
+        var bodySignalExtensionsClass = CreateSignalExtensionsClass(allMethods);
+        File.WriteAllText(SignalExtensionsFile, bodySignalExtensionsClass);
+    }
+
+    private void WriteSignalConstantsClass(List<GodotClass> classes) {
+        List<string> allMethods = classes
+            .Where(godotClass => godotClass.Signals.Count > 0 &&
+                                 !godotClass.IsEditor)
+            .SelectMany(godotClass => godotClass.Signals)
+            .Select(CreateSignalConstantField)
+            .ToList();
+        var bodySignalConstantsClass = CreateSignalConstantsClass(allMethods);
+        File.WriteAllText(SignalConstantsFile, bodySignalConstantsClass);
+    }
+
+    private void WriteAllActionClasses(List<GodotClass> classes) {
+        classes.Where(godotClass => godotClass.AllSignals.Count > 0 &&
+                                    !godotClass.IsEditor &&
+                                    !godotClass.IsStatic &&
+                                    !godotClass.IsAbstract)
+            .ToList()
+            .ForEach(WriteClass);
+    }
+
+    private static List<GodotClass> LoadGodotClasses() {
+        var classes = ClassDB.GetClassList()
+            .Select(className => new GodotClass(className))
+            .Where(godotClass => godotClass.IsValid)
+            .OrderBy(godotClass => godotClass.class_name)
+            .ToList();
+        return classes;
+    }
+
+    private void WriteClass(GodotClass godotClass) {
+        var bodyClass = CreateActionClass(godotClass);
+        File.WriteAllText(ActionClassesPath + "/" + godotClass.GeneratedClassName + ".cs", bodyClass);
+    }
+
+    private string CreateSignalExtensionMethod(Signal signal) {
+        var targetParam = signal.GodotClass.IsStatic
+            ? ""
+            : $"this {signal.GodotClass.FullClassName} target, ";
+        var target = signal.GodotClass.IsStatic ? $"{signal.GodotClass.ClassName}.Singleton" : "target";
+        return $@"
+        public static SignalHandler{signal.Generics()} On{signal.MethodName}({targetParam}Action{signal.Generics()} action) =>
+            new SignalHandler{signal.Generics()}({target}, SignalConstants.{signal.SignalCsharpConstantName}, action);";
+    }
+
+    private string CreateSignalConstantField(Signal signal) {
+        return $@"        public const string {signal.SignalCsharpConstantName} = ""{signal.signal_name}"";";
+    }
+
+    private string CreateActionMethod(Signal signal) {
+        var actionVarName = $"_on{signal.MethodName}Action";
+        var godotExecuteActionMethodName = $"Execute{signal.MethodName}";
+        return $@"
+        private Action{signal.Generics()}? {actionVarName}; 
+        public {signal.GodotClass.GeneratedClassName} On{signal.MethodName}(Action{signal.Generics()} action) {{
+            if ({actionVarName} == null) 
+                Connect(""{signal.signal_name}"", this, nameof({godotExecuteActionMethodName}));
+            {actionVarName} = action;
+            return this;
+        }}
+        public {signal.GodotClass.GeneratedClassName} RemoveOn{signal.MethodName}() {{
+            if ({actionVarName} == null) return this; 
+            Disconnect(""{signal.signal_name}"", this, nameof({godotExecuteActionMethodName}));
+            {actionVarName} = null;
+            return this;
+        }}
+        private void {godotExecuteActionMethodName}({signal.GetParamNamesWithType()}) =>
+            {actionVarName}?.Invoke({signal.GetParamNames()});
+        ";
+    }
+
+    private string CreateActionClass(GodotClass godotClass) {
+        var methods = godotClass.AllSignals.Select(CreateActionMethod);
+        var className = godotClass.GeneratedClassName;
+        var extends = godotClass.FullClassName;
+        var nodeMethods = godotClass.IsNode ? CreateNodeMethods(className) : "";
+        return $@"using System;
+using Godot;
+using System.Collections.Generic;
+using Environment = Godot.Environment;
+using Animation = Godot.Animation;
+using Object = Godot.Object;
+
+namespace {ActionClassNamespace} {{
+    public class {className} : {extends} {{
+{nodeMethods}
+{string.Join("\n", methods)}
+    }}
+}}";
+    }
+
+    private string CreateSignalExtensionsClass(IEnumerable<string> methods) {
+        return $@"using System;
+using Godot;
+using Object = Godot.Object;
+using Animation = Godot.Animation;
+
+namespace Betauer {{
+    public static partial class SignalExtensions {{
+{string.Join("\n", methods)}
+    }}
+}}";
+    }
+
+    private string CreateSignalConstantsClass(IEnumerable<string> methods) {
+        return $@"using System;
+namespace Betauer {{
+    public static partial class SignalConstants {{
+{string.Join("\n", methods)}
+    }}
+}}";
+    }
+
+    private string CreateNodeMethods(string className) {
+        return $@"
+        private List<Action<float>>? _onProcessAction; 
+        private List<Action<float>>? _onPhysicsProcess; 
+        private List<Action<InputEvent>>? _onInput; 
+        private List<Action<InputEvent>>? _onUnhandledInput; 
+        private List<Action<InputEventKey>>? _onUnhandledKeyInput;
+
+        public {className} OnProcessAction(Action<float> action) {{
+            _onProcessAction ??= new List<Action<float>>(1);
+            _onProcessAction.Add(action);
+            SetProcess(true);
+            return this;
+        }}
+        public {className} OnPhysicsProcess(Action<float> action) {{
+            _onPhysicsProcess ??= new List<Action<float>>(1);
+            _onPhysicsProcess.Add(action);
+            SetPhysicsProcess(true);
+            return this;
+        }}
+
+        public {className} OnInput(Action<InputEvent> action) {{
+            _onInput ??= new List<Action<InputEvent>>(1);
+            _onInput.Add(action);
+            SetProcessInput(true);
+            return this;
+        }}
+
+        public {className} OnUnhandledInput(Action<InputEvent> action) {{
+            _onUnhandledInput ??= new List<Action<InputEvent>>(1);
+            _onUnhandledInput.Add(action);
+            SetProcessUnhandledInput(true);
+            return this;
+        }}
+
+        public {className} OnUnhandledKeyInput(Action<InputEventKey> action) {{
+            _onUnhandledKeyInput ??= new List<Action<InputEventKey>>(1);
+            _onUnhandledKeyInput.Add(action);
+            SetProcessUnhandledKeyInput(true);
+            return this;
+        }}
+
+        public {className} RemoveOnProcessAction(Action<float> action) {{
+            _onProcessAction?.Remove(action);
+            return this;
+        }}
+
+        public {className} RemoveOnPhysicsProcess(Action<float> action) {{
+            _onPhysicsProcess?.Remove(action);
+            return this;
+        }}
+
+        public {className} RemoveOnInput(Action<InputEvent> action) {{
+            _onInput?.Remove(action);
+            return this;
+        }}
+
+        public {className} RemoveOnUnhandledInput(Action<InputEvent> action) {{
+            _onUnhandledInput?.Remove(action);
+            return this;
+        }}
+
+        public {className} RemoveOnUnhandledKeyInput(Action<InputEventKey> action) {{
+            _onUnhandledKeyInput?.Remove(action);
+            return this;
+        }}
+
+        public override void _Process(float delta) {{
+            if (_onProcessAction == null) {{
+                SetProcess(false);
+                return;
+            }}
+            for (var i = 0; i < _onProcessAction.Count; i++) _onProcessAction[i].Invoke(delta);
+        }}
+
+        public override void _PhysicsProcess(float delta) {{
+            if (_onPhysicsProcess == null) {{
+                SetPhysicsProcess(true);
+                return;
+            }}
+            for (var i = 0; i < _onPhysicsProcess.Count; i++) _onPhysicsProcess[i].Invoke(delta);
+        }}
+
+        public override void _Input(InputEvent @event) {{
+            if (_onInput == null) {{
+                SetProcessInput(true);
+                return;
+            }}
+            for (var i = 0; i < _onInput.Count; i++) _onInput[i].Invoke(@event);
+        }}
+
+        public override void _UnhandledInput(InputEvent @event) {{
+            if (_onUnhandledInput == null) {{
+                SetProcessUnhandledInput(true);
+                return;
+            }}
+            for (var i = 0; i < _onUnhandledInput.Count; i++) _onUnhandledInput[i].Invoke(@event);
+        }}
+
+        public override void _UnhandledKeyInput(InputEventKey @event) {{
+            if (_onUnhandledKeyInput == null) {{
+                SetProcessUnhandledKeyInput(true);
+                return;
+            }}
+            for (var i = 0; i < _onUnhandledKeyInput.Count; i++) _onUnhandledKeyInput[i].Invoke(@event);
+        }}";
+    }
+}
+
+public static class Tools {
+    public static string CamelCase(string name) =>
+        name.Split(new[] { "_" }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(s => char.ToUpperInvariant(s[0]) + s.Substring(1, s.Length - 1))
+            .Aggregate(string.Empty, (s1, s2) => s1 + s2);
+}
---
+++ Betauer.Generator/GodotClass.cs
@@ -0,0 +1,178 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Godot;
+using Godot.Collections;
+using Array = Godot.Collections.Array;
+
+public class Signal {
+    public readonly string signal_name;
+    public readonly string SignalName;
+    public readonly string SignalCsharpConstantName;
+    public readonly GodotClass GodotClass;
+    public readonly string MethodName;
+    public List<SignalArg> Args = new List<SignalArg>();
+
+    public Signal(GodotClass godotClass, string name) {
+        GodotClass = godotClass;
+        signal_name = name; // "on_button_pressed"
+        // Some signals include a "on_" prefix. The camel case version will not include it
+        SignalName = Tools.CamelCase(name.StartsWith("on") ? name.Substring(2) : name); // "ButtonPressed"
+        // IsStatic "VirtualServerButtonPressed"
+        // !IsStatic "ButtonPressed"
+        MethodName = GodotClass.IsStatic ? $"{GodotClass.ClassName}{SignalName}" : SignalName;
+        // const VirtualServer_ButtonPressedSignal = "pressed_signal"
+        SignalCsharpConstantName = $"{GodotClass.ClassName}_{MethodName}Signal";
+    }
+
+    public string GetParamNames() {
+        return string.Join(", ", Args.Select(arg => arg.ArgName).ToList());
+    }
+
+    public string GetParamNamesWithType() {
+        return string.Join(", ", Args.Select(arg => arg.Type + " " + arg.ArgName).ToList());
+    }
+
+    public string Generics() {
+        return Args.Count == 0 ? "" : $@"<{string.Join(", ", Args.Select(arg => arg.Type))}>";
+    }
+}
+
+public class GodotClass {
+    private static readonly System.Collections.Generic.Dictionary<string, string> ClassMap =
+        new System.Collections.Generic.Dictionary<string, string> {
+            { "_VisualScriptEditor", "VisualScriptEditor" },
+            { "TCPServer", "TCP_Server" },
+        };
+
+    private static readonly System.Collections.Generic.Dictionary<string, string> ReservedNames =
+        new System.Collections.Generic.Dictionary<string, string> {
+            { "event", "@event" },
+            { "object", "@object" },
+        };
+
+    private static readonly List<string> ClassStatic = new List<string> {
+        "AudioServer",
+        "ARVRServer",
+        "CameraServer",
+        "Input",
+        "VisualServer",
+        "VisualScriptEditor"
+    };
+
+    private static readonly List<string> EditorClasses = new List<string> {
+        "EditorFileDialog",
+        "EditorFileSystem",
+        "EditorInspector",
+        "EditorSelection",
+        "EditorSettings",
+        "EditorPlugin",
+        "EditorProperty",
+        "EditorResourcePreview",
+        "EditorResourcePicker",
+        "FileSystemDock",
+        "ScriptEditor",
+        "ScriptCreateDialog",
+        "VisualScriptEditor"
+    };
+
+    private static readonly System.Collections.Generic.Dictionary<int, string> TypeMap =
+        new System.Collections.Generic.Dictionary<int, string> {
+            { 0, "object" },
+            { 1, "bool" },
+            { 2, "int" },
+            { 3, "float" },
+            { 4, "string" },
+            { 5, "Vector2" },
+            { 6, "Rect2" },
+            { 7, "Vector3" },
+            { 8, "Transform2D" },
+            { 9, "Plane" },
+            { 10, "Quat" },
+            { 11, "AABB" },
+            { 12, "Basis" },
+            { 13, "Transform" },
+            { 14, "Color" },
+            { 15, "NodePath" },
+            { 16, "RID" },
+            { 17, "Object" },
+            { 18, "Godot.Collections.Dictionary" },
+            { 19, "Godot.Collections.Array" },
+            { 20, "byte[]" },
+            { 21, "int[]" },
+            { 22, "float[]" },
+            { 23, "string[]" },
+            { 24, "Vector2[]" },
+            { 25, "Vector3[]" },
+            { 26, "Color[]" }
+        };
+
+
+    public readonly string class_name;
+    public readonly string ClassName;
+    public readonly string FullClassName;
+    public readonly string GeneratedClassName;
+    public readonly bool IsStatic;
+    public readonly bool IsEditor;
+    public readonly bool IsAbstract;
+    public readonly bool IsValid;
+    public readonly bool IsNode;
+    public readonly List<Signal> Signals;
+    public readonly List<Signal> AllSignals;
+    public readonly Type Type;
+
+    public GodotClass(string className) {
+        class_name = className;
+        var camelCase = Tools.CamelCase(className);
+        ClassName = ClassMap.ContainsKey(camelCase) ? ClassMap[camelCase] : camelCase;
+        GeneratedClassName = ClassName + "Action";
+        IsStatic = ClassStatic.Contains(ClassName);
+        IsEditor = EditorClasses.Contains(ClassName);
+        FullClassName = ClassName;
+        IsValid = ClassDB.IsClassEnabled(className);
+        if (IsValid && !IsEditor && !IsStatic) {
+            var godotSharpAssemblyName = typeof(Node).Assembly.GetName().Name;
+            Type = Type.GetType("Godot." + ClassName + ", " + godotSharpAssemblyName);
+            if (Type != null) {
+                IsAbstract = Type.IsAbstract;
+                IsNode = Type.IsSubclassOf(typeof(Node));
+            } else {
+                IsValid = false;
+            }
+        }
+        if (IsValid) {
+            Signals = GetSignalsFromClass(ClassDB.ClassGetSignalList(class_name, true));
+            AllSignals = GetSignalsFromClass(ClassDB.ClassGetSignalList(class_name, false));
+        }
+    }
+
+    public List<Signal> GetSignalsFromClass(Array classDbSignals) {
+        List<Signal> signals = new List<Signal>();
+        foreach (Dictionary signalData in classDbSignals) {
+            var signalName = (string)signalData["name"];
+            var signalArgs = (Array)signalData["args"];
+            var signal = new Signal(this, signalName);
+            var args = new List<SignalArg>();
+            foreach (Dictionary arg in signalArgs) {
+                string? argClassName = (string)arg["class_name"];
+                var argName = (string)arg["name"];
+                var argType = TypeMap[(int)arg["type"]];
+                argName = ReservedNames.ContainsKey(argName) ? ReservedNames[argName] : argName;
+                args.Add(new SignalArg(argClassName?.Length > 0 ? argClassName : argType, argName));
+            }
+            signal.Args = args.OrderBy(arg => arg.ArgName).ToList();
+            signals.Add(signal);
+        }
+        return signals.OrderBy(signal => signal.SignalName).ToList();
+    }
+}
+
+public class SignalArg {
+    public string Type;
+    public string ArgName;
+
+    public SignalArg(string type, string argName) {
+        Type = type;
+        ArgName = argName;
+    }
+}
