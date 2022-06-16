4b065a814362ec0636602604a410132dcd77fe93
Alberto Vilches <194074+avilches@users.noreply.github.com>
2022-06-16 15:31:30 +0200

    Specify assemblies in Betauer tests

--- TestRunner.cs
+++ TestRunner.cs
@@ -1,20 +1,20 @@
+using System.Linq;
+using System.Reflection;
 using Betauer;
-using Betauer.Animation.Tests;
-using Betauer.DI.Tests;
-using Betauer.StateMachine.Tests;
 using Betauer.TestRunner;
-using Betauer.Tests;
 using Godot;
 
 public class TestRunner : SceneTree {
     public override async void _Initialize() {
         LoggerFactory.LoadFrames(GetFrame);
         LoggerFactory.SetDefaultTraceLevel(TraceLevel.All);
-        typeof(ActionTweenTests).GetType();
-        typeof(VariantHelperTests).GetType();
-        typeof(ScannerBasicTests).GetType();
-        typeof(GodotTopicTests).GetType();
-        typeof(StateMachineTests).GetType();
-        await ConsoleTestRunner.RunTests(this);
+        var assemblies = new[] {
+            "Betauer.Animation.Tests",
+            "Betauer.Core.Tests",
+            "Betauer.DI.Tests",
+            "Betauer.GameTools.Tests",
+            "Betauer.StateMachine.Tests",
+        }.Select(Assembly.Load).ToArray();
+        await ConsoleTestRunner.RunTests(this, assemblies);
     }
 }
