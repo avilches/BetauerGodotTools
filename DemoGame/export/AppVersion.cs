c2297f4d9bea73eb825369c4f227e4ace13b6cd9
Alberto Vilches <194074+avilches@users.noreply.github.com>
2022-06-17 14:41:54 +0200

    release.properties includes NAME and AUTHOR, used by export_presets.cfg

--- DemoGame/export/AppVersion.cs
+++ DemoGame/export/AppVersion.cs
@@ -1,16 +1,23 @@
 using System;
 using System.Linq;
 using Godot;
+using Godot.Collections;
 
 namespace Veronenger.export {
     public class AppVersion : MainLoop {
         private static readonly string[] ExcludeFilter = { "export/*", "Tests/*" };
         
         public override void _Initialize() {
-            var version = System.IO.File.ReadAllText("./export/app-version.txt");
+            var release = ReadProperties("./export/release.properties");
+            
+            // TODO: add these to project.godot so it can be loaded inside the game
+            var version = release["VERSION"];
+            var author = release["AUTHOR"];
+            var name = release["NAME"];
+            
             FixProjectGodot();
             SetAppVersionProjectGodot(version);
-            FixPresetsVersion(version);
+            FixPresetsVersion(name, author, version);
             FixPresetsExcludeFilter();
         }
 
@@ -51,7 +58,7 @@ namespace Veronenger.export {
             cf.Save("res://export_presets.cfg");
         }
 
-        private static void FixPresetsVersion(string version) {
+        private static void FixPresetsVersion(string name, string author, string version) {
             var cf = new ConfigFile();
             cf.Load("res://export_presets.cfg");
             
@@ -71,10 +78,21 @@ namespace Veronenger.export {
                     .First(section => (string)cf.GetValue(section, "name") == name);
 
             var osx = FindPreset("Mac OSX");
+            // TODO: add a unique id to release.properties
+            cf.SetValue(osx + ".options", "application/identifier", author + "." + name);
+            cf.SetValue(osx + ".options", "application/name", name);
+            // TODO: add a description to release.properties
+            cf.SetValue(osx + ".options", "application/info", name);
             cf.SetValue(osx + ".options", "application/short_version", version);
+            cf.SetValue(osx + ".options", "application/copyright", author);
             cf.SetValue(osx + ".options", "application/version", version);
 
             var win = FindPreset("Windows Desktop");
+            cf.SetValue(win + ".options", "application/product_name", name);
+            // TODO: add a description to release.properties
+            cf.SetValue(win + ".options", "application/file_description", name);
+            cf.SetValue(win + ".options", "application/company_name", author);
+            cf.SetValue(win + ".options", "application/copyright", author);
             cf.SetValue(win + ".options", "application/file_version", version);
             cf.SetValue(win + ".options", "application/product_version", version);
             
@@ -98,6 +116,19 @@ namespace Veronenger.export {
                 }
             }
             return string.Join(", ", values);
+        }
+
+
+        Dictionary<string, string> ReadProperties(string propertiesFile) {
+            var data = new Dictionary<string, string>();
+            var lines = System.IO.File.ReadAllLines(propertiesFile)
+                .Where(filter => !string.IsNullOrEmpty(filter));
+            foreach (var row in lines) {
+                var key = row.Split('=')[0].Trim();
+                var value = row.Split('=')[1].Trim();
+                data.Add(key, value);
+            }
+            return data;
         }
 
         public override bool _Idle(float delta) => true;
