319047b51f1e19cd4d141f10eb286b46b1898318
Alberto Vilches <194074+avilches@users.noreply.github.com>
2022-06-24 17:20:24 +0200

    Removed BezierCurve (fake) cache. It was totally useless!

--- Betauer.Animation/Templates.cs
+++ Betauer.Animation/Templates.cs
@@ -5,12 +5,7 @@ namespace Betauer.Animation {
         private static readonly Dictionary<string, BezierCurve> CurveBeziersCache = new Dictionary<string, BezierCurve>();
 
         private static BezierCurve Bezier(float p1x, float p1y, float p2x, float p2y) {
-            var bezierCurve = BezierCurve.Create(p1x, p1y, p2x, p2y);
-            if (CurveBeziersCache.ContainsKey(bezierCurve.Name)) {
-                return CurveBeziersCache[bezierCurve.Name];
-            }
-            CurveBeziersCache[bezierCurve.Name] = bezierCurve;
-            return bezierCurve;
+            return BezierCurve.Create(p1x, p1y, p2x, p2y);
         }
     }
 }
