b68a24408c4d680cd042a278bfd2231257a44024
Alberto Vilches <194074+avilches@users.noreply.github.com>
2022-07-01 19:35:28 +0200

    Experimental: dynamic signals + tests

---
+++ Betauer.Core.Tests/Experimental/DynamicSignalTests.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Betauer.Experimental;
+using Betauer.TestRunner;
+using Godot;
+using NUnit.Framework;
+
+namespace Betauer.Tests.Experimental {
+    [TestFixture]
+    public class DynamicSignalTests : Node {
+        [Test(Description = "0p and 1p signals")]
+        [Only]
+        public async Task BasicTest() {
+            DynamicSignalExtensions.Manager = new DynamicSignalManager();
+            var b1 = new CheckButton();
+            var b2 = new CheckButton();
+            AddChild(b1);
+            AddChild(b2);
+            await this.AwaitIdleFrame();
+            var executed1 = 0;
+            var executed2 = 0;
+            var toggled1 = new List<bool>();
+            var toggled2 = new List<bool>();
+
+            DynamicSignal d1 = DynamicSignalExtensions.OnPressed(b1,() => {
+                executed1++;
+            });
+            DynamicSignalExtensions.OnPressed(b2,() => {
+                executed2++;
+            });
+
+            DynamicSignalExtensions.OnToggled(b1,(tog) => {
+                toggled1.Add(tog);
+            });
+            DynamicSignalExtensions.OnToggled(b2,(tog) => {
+                toggled2.Add(tog);
+            });
+            
+            b1.EmitSignal("pressed");
+            Assert.That(executed1, Is.EqualTo(1));
+            Assert.That(executed2, Is.EqualTo(0));
+            Assert.That(toggled1, Is.EqualTo(new bool[]{}));
+            Assert.That(toggled2, Is.EqualTo(new bool[]{}));
+
+            b2.EmitSignal("pressed");
+            Assert.That(executed1, Is.EqualTo(1));
+            Assert.That(executed2, Is.EqualTo(1));
+            Assert.That(toggled1, Is.EqualTo(new bool[]{}));
+            Assert.That(toggled2, Is.EqualTo(new bool[]{}));
+
+            b1.Pressed = true;
+            Assert.That(executed1, Is.EqualTo(1));
+            Assert.That(executed2, Is.EqualTo(1));
+            Assert.That(toggled1.ToArray(), Is.EqualTo(new []{true}));
+            Assert.That(toggled2.ToArray(), Is.EqualTo(new bool[]{}));
+            
+            b2.Pressed = true;
+            Assert.That(executed1, Is.EqualTo(1));
+            Assert.That(executed2, Is.EqualTo(1));
+            Assert.That(toggled1.ToArray(), Is.EqualTo(new []{true}));
+            Assert.That(toggled2.ToArray(), Is.EqualTo(new []{true}));
+
+            b1.Pressed = false;
+            b2.Pressed = false;
+            Assert.That(executed1, Is.EqualTo(1));
+            Assert.That(executed2, Is.EqualTo(1));
+            Assert.That(toggled1.ToArray(), Is.EqualTo(new []{true, false}));
+            Assert.That(toggled2.ToArray(), Is.EqualTo(new []{true, false}));
+            
+            Assert.That(DynamicSignalExtensions.Manager.Signals0Ps.Count, Is.EqualTo(2));            
+            Assert.That(DynamicSignalExtensions.Manager.Signals1Ps.Count, Is.EqualTo(2));            
+            Assert.That(DynamicSignalExtensions.Manager.SignalsList.Count, Is.EqualTo(4));
+
+            DynamicSignalExtensions.Manager.Clean();
+            
+            Assert.That(DynamicSignalExtensions.Manager.Signals0Ps.Count, Is.EqualTo(2));            
+            Assert.That(DynamicSignalExtensions.Manager.Signals1Ps.Count, Is.EqualTo(2));            
+            Assert.That(DynamicSignalExtensions.Manager.SignalsList.Count, Is.EqualTo(4));
+
+            DynamicSignalExtensions.Manager.Remove(d1);
+            Assert.That(DynamicSignalExtensions.Manager.Signals0Ps.Count, Is.EqualTo(1));            
+            Assert.That(DynamicSignalExtensions.Manager.Signals1Ps.Count, Is.EqualTo(2));            
+            Assert.That(DynamicSignalExtensions.Manager.SignalsList.Count, Is.EqualTo(3));
+
+        }
+
+        [Test]
+        [Only]
+        public async Task OneShot() {
+            DynamicSignalExtensions.Manager = new DynamicSignalManager();
+            var b1 = new CheckButton();
+            var b2 = new CheckButton();
+            AddChild(b1);
+            AddChild(b2);
+            await this.AwaitIdleFrame();
+            var executedNormal = 0;
+            var executedOneShot = 0;
+
+            DynamicSignalExtensions.OnPressed(b1,() => { executedNormal++; });
+            DynamicSignalExtensions.OnPressed(b2, () => { executedOneShot++; }, true);
+            b1.EmitSignal("pressed");
+            b2.EmitSignal("pressed");
+            Assert.That(executedNormal, Is.EqualTo(1));
+            Assert.That(executedOneShot, Is.EqualTo(1));
+
+            b1.EmitSignal("pressed");
+            b2.EmitSignal("pressed");
+            Assert.That(executedNormal, Is.EqualTo(2));
+            Assert.That(executedOneShot, Is.EqualTo(1));
+            
+            Assert.That(DynamicSignalExtensions.Manager.Signals0Ps.Count, Is.EqualTo(2));            
+            Assert.That(DynamicSignalExtensions.Manager.Signals1Ps.Count, Is.EqualTo(0));            
+            Assert.That(DynamicSignalExtensions.Manager.SignalsList.Count, Is.EqualTo(2));
+
+            DynamicSignalExtensions.Manager.Clean();
+            
+            Assert.That(DynamicSignalExtensions.Manager.Signals0Ps.Count, Is.EqualTo(1));            
+            Assert.That(DynamicSignalExtensions.Manager.Signals1Ps.Count, Is.EqualTo(0));            
+            Assert.That(DynamicSignalExtensions.Manager.SignalsList.Count, Is.EqualTo(1));
+
+
+        }
+    }
+}
---
+++ Betauer.Core/Experimental/DynamicSignal.cs
@@ -0,0 +1,53 @@
+using System;
+using Godot;
+
+namespace Betauer.Experimental {
+    public abstract class DynamicSignal {
+        public readonly Node Node;
+        public readonly string Signal;
+        public readonly bool OneShot = false;
+
+        private bool _valid = true;
+
+        protected DynamicSignal(Node node, string signal, bool oneShot) {
+            Node = node;
+            Signal = signal;
+            OneShot = oneShot;
+        }
+
+        public bool IsValid() => _valid && Godot.Object.IsInstanceValid(Node);
+
+        internal void Consume() {
+            if (OneShot) _valid = false;
+        }
+
+        internal static string CreateKey(ulong instanceId, string signal, int actionHashCode) {
+            return "" + instanceId + signal + actionHashCode;
+        }
+    }
+
+    public abstract class DynamicSignal<T> : DynamicSignal {
+        internal readonly T Action;
+
+        protected DynamicSignal(Node node, string signal, T action, bool oneShot) : base(node, signal, oneShot) {
+            Action = action;
+        }
+
+        public string GetKey() {
+            return CreateKey(Node.GetInstanceId(), Signal, Action.GetHashCode());
+        }
+    }
+
+    public class DynamicSignal0P : DynamicSignal<Action> {
+        public DynamicSignal0P(Node node, string signal, Action action, bool oneShot) : base(node, signal, action,
+            oneShot) {
+        }
+    }
+
+    public class DynamicSignal1P : DynamicSignal<Action<object>> {
+        public DynamicSignal1P(Node node, string signal, Action<object> action, bool oneShot) : base(node, signal,
+            action,
+            oneShot) {
+        }
+    }
+}
