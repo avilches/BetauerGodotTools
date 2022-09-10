fca5e2d7c9464c43fe1465aadc8faaf577e90406
Alberto Vilches <194074+avilches@users.noreply.github.com>
2022-09-10 11:19:51 +0200

    ObjectConsumer.cs: renamed Count -> Size + Added PeakSize

--- Betauer.Core/Memory/ObjectConsumer.cs
+++ Betauer.Core/Memory/ObjectConsumer.cs
@@ -10,7 +10,8 @@ namespace Betauer.Memory {
         private static readonly Logger Logger = LoggerFactory.GetLogger(typeof(Consumer));
         private readonly HashSet<IObjectConsumer> _objects = new HashSet<IObjectConsumer>();
 
-        public int Count => _objects.Count;
+        public int PeakSize { get; private set; }
+        public int Size => _objects.Count;
 
         public List<IObjectConsumer> ToList() {
             lock (_objects) return new List<IObjectConsumer>(_objects);
@@ -20,11 +21,9 @@ namespace Betauer.Memory {
             lock (_objects) {
                 return _objects.RemoveWhere(o => {
                     var consumed = o.Consume(force);
-#if DEBUG
-                    if (consumed) {
-                        Logger.Debug($"Count: {_objects.Count - 1} | - {o}");
-                    }
-#endif
+                    #if DEBUG
+                    if (consumed) Logger.Debug($"Size: {_objects.Count - 1} | - {o}");
+                    #endif
                     return consumed;
                 });
             }
@@ -32,10 +31,11 @@ namespace Betauer.Memory {
 
         public void Add(IObjectConsumer o) {
             lock (_objects) {
-#if DEBUG
-                Logger.Debug($"Count: {_objects.Count + 1} | + {o}");
-#endif
+                #if DEBUG
+                Logger.Debug($"Size: {_objects.Count + 1} | + {o}");
+                #endif
                 _objects.Add(o);
+                PeakSize = Math.Max(PeakSize, _objects.Count);
             }
         }
 
@@ -43,11 +43,9 @@ namespace Betauer.Memory {
             lock (_objects) {
                 _objects.RemoveWhere(x => {
                     var matches = x == o;
-#if DEBUG
-                    if (matches) {
-                        Logger.Debug($"Count: {_objects.Count - 1} | - {o}");
-                    }
-#endif
+                    #if DEBUG
+                    if (matches) Logger.Debug($"Size: {_objects.Count - 1} | - {o}");
+                    #endif
                     return matches;
                 });
             }
