254b14c565beb71be3a57b0e7545ac89d96ca521
Alberto Vilches <194074+avilches@users.noreply.github.com>
2022-09-23 15:28:46 +0200

    CharacterManager.cs: no more ObjectDisposable for signals

--- DemoGame/Game/Managers/CharacterManager.cs
+++ DemoGame/Game/Managers/CharacterManager.cs
@@ -1,16 +1,14 @@
 using System;
 using Godot;
 using Betauer.DI;
-using Betauer.Memory;
 using Betauer.Signal;
 using Betauer.Signal.Bus;
 using Veronenger.Game.Controller.Character;
 using static Veronenger.Game.LayerConstants;
-using Array = Godot.Collections.Array;
 
 namespace Veronenger.Game.Managers {
     [Service]
-    public class CharacterManager : DisposableGodotObject /* needed to receive signals TODO: it shouldn't be! */{
+    public class CharacterManager {
         private const string GROUP_ENEMY = "enemy";
 
         public PlayerController PlayerController { get; private set; }
@@ -65,28 +63,14 @@ namespace Veronenger.Game.Managers {
             deathArea2D.CollisionMask = 0;
             // TODO: this should be a topic, so other places can subscribe like remove all bullets
             deathArea2D.SetCollisionLayerBit(LayerPlayerStageDetector, true);
-            deathArea2D.Connect(SignalConstants.Area2D_AreaEnteredSignal, this, nameof(PlayerEnteredDeathZone),
-                new Array { deathArea2D });
-        }
-
-        public void PlayerEnteredDeathZone(Area2D playerDetector, Area2D deathArea2D) {
-            // TODO Send and event instead ?
-            GD.Print("a");
-            // PlayerController.DeathZone(deathArea2D);
+            deathArea2D.OnAreaEntered((player) => GD.Print("Muerto"));
         }
 
         public void ConfigureSceneChange(Area2D sceneChangeArea2D, string scene) {
-            sceneChangeArea2D.Connect(SignalConstants.Area2D_AreaEnteredSignal, this,
-                nameof(_on_player_entered_scene_change),
-                new Array { sceneChangeArea2D, scene });
             sceneChangeArea2D.CollisionLayer = 0;
             sceneChangeArea2D.CollisionMask = 0;
             sceneChangeArea2D.SetCollisionLayerBit(LayerPlayerStageDetector, true);
-        }
-
-        public void _on_player_entered_scene_change(Area2D player, Area2D stageEnteredArea2D, string scene) {
-            // TODO: Send an event instead?
-            GameManager.QueueChangeSceneWithPlayer(scene);
+            sceneChangeArea2D.OnAreaEntered((player) => GameManager.QueueChangeSceneWithPlayer(scene));
         }
     }
 }
