2d666cc360e295898666c2cccff1204f4fd55374
Alberto Vilches <194074+avilches@users.noreply.github.com>
2022-10-20 08:39:17 +0200

    [Stable] PlatformBody refactor: added wall collision + monitor collision + refactor/cleanup + moved the MoveAnd*() reminder to the state machine

--- DemoGame/Game/Managers/Autoload/Bootstrap.cs
+++ DemoGame/Game/Managers/Autoload/Bootstrap.cs
@@ -132,13 +132,9 @@ namespace Veronenger.Game.Managers.Autoload {
             LoggerFactory.SetTraceLevel(typeof(PlayerController), TraceLevel.Error);
             LoggerFactory.SetTraceLevel("Player.StateMachine", TraceLevel.Error);
             LoggerFactory.SetTraceLevel("Player.AnimationStack", TraceLevel.Error);
-            LoggerFactory.SetTraceLevel("Player.Collision", TraceLevel.All);
-            LoggerFactory.SetTraceLevel("Player.Motion", TraceLevel.Error);
             LoggerFactory.SetTraceLevel(typeof(ZombieController), TraceLevel.Error);
             LoggerFactory.SetTraceLevel("Zombie.StateMachine", TraceLevel.Error);
             LoggerFactory.SetTraceLevel("Zombie.AnimationStack", TraceLevel.Error);
-            LoggerFactory.SetTraceLevel("Zombie.Collision", TraceLevel.Error);
-            LoggerFactory.SetTraceLevel("Zombie.Motion", TraceLevel.Error);
             
             // LoggerFactory.OverrideTraceLevel(TraceLevel.All);
         }
