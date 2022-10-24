d431b4620f7f8935c1990528915aadbe4715fcf7
Alberto Vilches <194074+avilches@users.noreply.github.com>
2022-10-24 15:38:18 +0200

    StringTools.cs. HumanReadableBytes format 0.00 instead of 0.##

--- Betauer.Core/StringTools.cs
+++ Betauer.Core/StringTools.cs
@@ -1,12 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using Betauer.Reflection;
-using Godot;
-using File = System.IO.File;
 
 namespace Betauer {
     public static class StringTools {
@@ -23,21 +15,21 @@ namespace Betauer {
                 return $"{numBytes.ToString()} B";
 
             if (numBytes < 1048576)
-                return $"{numBytes / 1024d:0.##} KB";
+                return $"{numBytes / 1024d:0.00} KB";
 
             if (numBytes < 1073741824)
-                return $"{numBytes / 1048576d:0.##} MB";
+                return $"{numBytes / 1048576d:0.00} MB";
 
             if (numBytes < 1099511627776)
-                return $"{numBytes / 1073741824d:0.##} GB";
+                return $"{numBytes / 1073741824d:0.00} GB";
 
             if (numBytes < 1125899906842624)
-                return $"{numBytes / 1099511627776d:0.##} TB";
+                return $"{numBytes / 1099511627776d:0.00} TB";
 
             if (numBytes < 1152921504606846976)
-                return $"{numBytes / 1125899906842624d:0.##} PB";
+                return $"{numBytes / 1125899906842624d:0.00} PB";
 
-            return $"{numBytes / 1152921504606846976d:0.##} EB";
+            return $"{numBytes / 1152921504606846976d:0.00} EB";
         }
 
         public static string FastFormatDateTime(DateTime dateTime, char dateSep = '-') {
