eb7508a1df2aba9f917aeaba208050ba149857d5
Alberto Vilches <194074+avilches@users.noreply.github.com>
2022-10-31 20:40:39 +0100

    EnumDictionary: refactor name + using Span<T> to improve speed looping

--- Betauer.Core/EnumDictionary.cs
+++ Betauer.Core/EnumDictionary.cs
@@ -4,41 +4,41 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace Betauer {
-    public class FixedEnumDictionary<TKey, TValue> : EnumDictionary<TKey, TValue> 
-        where TKey : Enum 
+    public class FixedEnumDictionary<TEnumKey, TValue> : EnumDictionary<TEnumKey, TValue> 
+        where TEnumKey : Enum 
         where TValue : class {
         
-        internal FixedEnumDictionary(TKey[] enums) : base(enums) {
+        internal FixedEnumDictionary(TEnumKey[] enums) : base(enums) {
         }
 
-        public override int GetPos(TKey key) => key.ToInt();
+        public override int GetPos(TEnumKey key) => key.ToInt();
     }
 
-    public class VariableEnumDictionary<TKey, TValue> : EnumDictionary<TKey, TValue> 
-        where TKey : Enum 
+    public class VariableEnumDictionary<TEnumKey, TValue> : EnumDictionary<TEnumKey, TValue> 
+        where TEnumKey : Enum 
         where TValue : class {
-        
-        internal VariableEnumDictionary(TKey[] enums) : base(enums) {
+        internal VariableEnumDictionary(TEnumKey[] enums) : base(enums) {
         }
 
-        public override int GetPos(TKey key) {
+        public override int GetPos(TEnumKey key) {
+            Span<TEnumKey> span = KeyArray.AsSpan();
             for (var pos = 0; pos < Count; pos++) {
-                if (EqualityComparer<TKey>.Default.Equals(KeyArray[pos], key)) return pos;
+                if (EqualityComparer<TEnumKey>.Default.Equals(span[pos], key)) return pos;
             }
             return -1; // this will never happen
         }
     }
 
-    public abstract class EnumDictionary<TKey, TValue> : IDictionary<TKey, TValue>
-        where TKey : Enum
+    public abstract class EnumDictionary<TEnumKey, TValue> : IDictionary<TEnumKey, TValue>
+        where TEnumKey : Enum
         where TValue : class {
         
+        public readonly TEnumKey[] KeyArray;
         public readonly TValue?[] ValueArray;
-        public readonly TKey[] KeyArray;
         public int Count { get; }
 
-        public static EnumDictionary<TKey, TValue> Create(Func<TKey, TValue>? filler = null) {
-            var enums = (TKey[])Enum.GetValues(typeof(TKey));
+        public static EnumDictionary<TEnumKey, TValue> Create(Func<TEnumKey, TValue>? filler = null) {
+            var enums = (TEnumKey[])Enum.GetValues(typeof(TEnumKey));
             var isFixed = true;
             for (var count = 0; count < enums.Length; count++) {
                 if (enums[count].ToInt() != count) {
@@ -47,27 +47,27 @@ namespace Betauer {
                 }
             }
             
-            EnumDictionary<TKey, TValue> enumDictionary = isFixed ? 
-                new FixedEnumDictionary<TKey, TValue>(enums) : 
-                new VariableEnumDictionary<TKey, TValue>(enums);
+            EnumDictionary<TEnumKey, TValue> enumDictionary = isFixed ? 
+                new FixedEnumDictionary<TEnumKey, TValue>(enums) : 
+                new VariableEnumDictionary<TEnumKey, TValue>(enums);
             
             if (filler != null) enumDictionary.Fill(filler);
             return enumDictionary;
         }
 
-        public abstract int GetPos(TKey key);
+        public abstract int GetPos(TEnumKey key);
 
-        protected EnumDictionary(TKey[] enums) {
+        protected EnumDictionary(TEnumKey[] enums) {
             KeyArray = enums;
             Count = enums.Length;
             ValueArray = new TValue[Count];
         }
 
-        public void Fill(Func<TKey, TValue> filler) {
+        public void Fill(Func<TEnumKey, TValue> filler) {
             foreach (var key in Keys) this[key] = filler(key);
         }
 
-        public TValue? this[TKey key] {
+        public TValue? this[TEnumKey key] {
             get => ValueArray[GetPos(key)];
             set => ValueArray[GetPos(key)] = value;
         }
@@ -76,60 +76,60 @@ namespace Betauer {
             return GetEnumerator();
         }
 
-        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() {
+        public IEnumerator<KeyValuePair<TEnumKey, TValue>> GetEnumerator() {
             return new Enumerator(this);
         }
 
-        public ICollection<TKey> Keys => KeyArray;
+        public ICollection<TEnumKey> Keys => KeyArray;
         public ICollection<TValue> Values => ValueArray;
 
         public void Clear() {
             for (var i = 0; i < ValueArray.Length; i++) ValueArray[i] = null;
         }
 
-        public void Add(KeyValuePair<TKey, TValue> item) {
+        public void Add(KeyValuePair<TEnumKey, TValue> item) {
             this[item.Key] = item.Value;
         }
 
-        public bool Contains(KeyValuePair<TKey, TValue> item) {
+        public bool Contains(KeyValuePair<TEnumKey, TValue> item) {
             return item.Value == null ? this[item.Key] == null : this[item.Key]?.Equals(item.Value) ?? false;
         }
 
-        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex) {
+        public void CopyTo(KeyValuePair<TEnumKey, TValue>[] array, int arrayIndex) {
             throw new NotImplementedException();
         }
 
-        public bool Remove(KeyValuePair<TKey, TValue> item) {
+        public bool Remove(KeyValuePair<TEnumKey, TValue> item) {
             this[item.Key] = null;
             return true;
         }
 
         public bool IsReadOnly => false;
 
-        public void Add(TKey key, TValue value) {
+        public void Add(TEnumKey key, TValue value) {
             this[key] = value;
         }
 
-        public bool ContainsKey(TKey key) {
+        public bool ContainsKey(TEnumKey key) {
             return this[key] is not null;
         }
 
-        public bool Remove(TKey key) {
+        public bool Remove(TEnumKey key) {
             this[key] = null;
             return true;
         }
 
-        public bool TryGetValue(TKey key, out TValue value) {
+        public bool TryGetValue(TEnumKey key, out TValue value) {
             value = this[key];
             return value is not null;
         }
 
-        public class Enumerator : IEnumerator<KeyValuePair<TKey, TValue>> {
-            private readonly EnumDictionary<TKey, TValue> _enumDictionary;
+        public class Enumerator : IEnumerator<KeyValuePair<TEnumKey, TValue>> {
+            private readonly EnumDictionary<TEnumKey, TValue> _enumDictionary;
             private int _index;
-            private KeyValuePair<TKey, TValue> _current;
+            private KeyValuePair<TEnumKey, TValue> _current;
 
-            internal Enumerator(EnumDictionary<TKey, TValue> enumDictionary) {
+            internal Enumerator(EnumDictionary<TEnumKey, TValue> enumDictionary) {
                 _enumDictionary = enumDictionary;
                 _index = 0;
                 _current = default;
@@ -139,9 +139,9 @@ namespace Betauer {
                 // Use unsigned comparison since we set index to dictionary.count+1 when the enumeration ends.
                 // dictionary.count+1 could be negative if dictionary.count is int.MaxValue
                 if ((uint)_index < (uint)_enumDictionary.Count) {
-                    TKey key = _enumDictionary.KeyArray[_index];
+                    TEnumKey key = _enumDictionary.KeyArray[_index];
                     TValue? value = _enumDictionary[key];
-                    _current = new KeyValuePair<TKey, TValue>(key, value);
+                    _current = new KeyValuePair<TEnumKey, TValue>(key, value);
                     _index++;
                     return true;
                 }
@@ -151,7 +151,7 @@ namespace Betauer {
                 return false;
             }
 
-            public KeyValuePair<TKey, TValue> Current => _current;
+            public KeyValuePair<TEnumKey, TValue> Current => _current;
 
             public void Dispose() {
             }
