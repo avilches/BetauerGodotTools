d9cd719a1a455c65878b5f97053165d187bb53a5
Alberto Vilches <194074+avilches@users.noreply.github.com>
2022-11-07 00:11:04 +0100

    GetTree().QuitSafely() can be called once (more times are ignored)

--- Betauer.Core/Nodes/SceneTreeExtensions.cs
+++ Betauer.Core/Nodes/SceneTreeExtensions.cs
@@ -1,14 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Betauer.Tools.Logging;
 using Godot;
-using Thread = System.Threading.Thread;
 
 namespace Betauer.Nodes {
     public static partial class SceneTreeExtensions {
+        private static bool _quitRequested = false;
         public static void QuitSafely(this SceneTree sceneTree, int exitCode = -1) {
+            if (_quitRequested) return;
+            _quitRequested = true;
             sceneTree.SetAutoAcceptQuit(false);
             sceneTree.Notification(MainLoop.NotificationWmQuitRequest);
+            LoggerFactory.SetAutoFlush(true);
             sceneTree.Quit(exitCode);
         }
     }
