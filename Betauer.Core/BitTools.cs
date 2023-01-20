4854cc92244ce5385b55bb9f16178f106941cffc
Alberto Vilches <194074+avilches@users.noreply.github.com>
2023-01-20 12:36:46 +0100

    BitTools: added SetBit(mask, bit, value) where if value is true -> EnableBet or false -> DisableBit

--- Betauer.Core/BitTools.cs
+++ Betauer.Core/BitTools.cs
@@ -1,11 +1,15 @@
-namespace Betauer.Core; 
+namespace Betauer.Core;
 
-public class BitTools {
+public static partial class BitTools {
+    public static uint SetBit(uint bits, uint position, bool value) =>
+        value ? EnableBit(bits, position) : DisableBit(bits, position);
+
     public static uint DisableBit(uint bits, uint position) {
         var mask = (uint)1 << (int)(position - 1);
         bits &= ~mask;
         return bits;
     }
+
     public static uint EnableBit(uint bits, uint position) {
         bits |= (uint)1 << (int)(position - 1);
         return bits;
