c60b8cdf886510edfcff185586cc18bf06a7bb4d
Alberto Vilches <194074+avilches@users.noreply.github.com>
2023-03-20 10:32:12 +0100

    missing [Test] attribute

--- Betauer.Core.Tests/BitToolsTests.cs
+++ Betauer.Core.Tests/BitToolsTests.cs
@@ -5,6 +5,7 @@ namespace Betauer.Core.Tests;
 [TestFixture]
 public class BitToolsTests {
 
+    [Test]
     public void EnableDisableHasTest() {
         for (var i = 1; i < 20; i++) {
             int bit = 0b00000000000000000000;
@@ -19,7 +20,5 @@ public class BitToolsTests {
             bit = BitTools.SetBit(0, i, false);
             Assert.That(BitTools.HasBit(bit, i), Is.False);
         }
-        
     }
-    
 }
