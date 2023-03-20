db54c70dcff8848f87eaa24d21c072272b08ed68
Alberto Vilches <194074+avilches@users.noreply.github.com>
2023-03-20 10:28:02 +0100

    Betauer.Core: BitTools, added HasBit + tests

---
+++ Betauer.Core.Tests/BitToolsTests.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+
+namespace Betauer.Core.Tests; 
+
+[TestFixture]
+public class BitToolsTests {
+
+    public void EnableDisableHasTest() {
+        for (var i = 1; i < 20; i++) {
+            int bit = 0b00000000000000000000;
+            Assert.That(BitTools.HasBit(bit, i), Is.False);
+            bit = BitTools.SetBit(0, i, true);
+            Assert.That(BitTools.HasBit(bit, i), Is.True);
+        }
+        
+        for (var i = 1; i < 20; i++) {
+            int bit = 0b11111111111111111111;
+            Assert.That(BitTools.HasBit(bit, i), Is.True);
+            bit = BitTools.SetBit(0, i, false);
+            Assert.That(BitTools.HasBit(bit, i), Is.False);
+        }
+        
+    }
+    
+}
--- Betauer.Core/BitTools.cs
+++ Betauer.Core/BitTools.cs
@@ -1,17 +1,24 @@
+using System;
+
 namespace Betauer.Core;
 
 public static partial class BitTools {
-    public static uint SetBit(uint bits, uint position, bool value) =>
+    public static int SetBit(int bits, int position, bool value) =>
         value ? EnableBit(bits, position) : DisableBit(bits, position);
 
-    public static uint DisableBit(uint bits, uint position) {
-        var mask = (uint)1 << (int)(position - 1);
+    public static bool HasBit(int bits, int position) {
+        var mask = 1 << (position - 1);
+        return (bits & mask) == mask;
+    }
+
+    public static int DisableBit(int bits, int position) {
+        var mask = 1 << (position - 1);
         bits &= ~mask;
         return bits;
     }
 
-    public static uint EnableBit(uint bits, uint position) {
-        bits |= (uint)1 << (int)(position - 1);
+    public static int EnableBit(int bits, int position) {
+        bits |= 1 << (position - 1);
         return bits;
     }
 }
