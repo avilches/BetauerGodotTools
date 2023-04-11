75623ac446e4d64ea0898a521c1bf9ab9cd5736e
Alberto Vilches <194074+avilches@users.noreply.github.com>
2023-04-11 10:50:06 +0200

    DemoGame: fix inventory + added Log()

--- DemoGame/Src/Character/Player/Inventory.cs
+++ DemoGame/Src/Character/Player/Inventory.cs
@@ -10,11 +10,12 @@ public class Inventory {
 
     public readonly List<Item> Items = new();
     public int Selected = 0;
-    public WeaponMeleeItem WeaponMeleeEquipped { get; private set; }
-    public WeaponRangeItem WeaponRangeEquipped { get; private set; }
+    public WeaponMeleeItem? WeaponMeleeEquipped { get; private set; }
+    public WeaponRangeItem? WeaponRangeEquipped { get; private set; }
     
     public void Pick(Item item) {
         Items.Add(item);
+        Log();
     }
 
     public void PickAndEquip(Item item) {
@@ -22,13 +23,23 @@ public class Inventory {
         Equip(Items.Count - 1);
     }
 
-    public Item GetCurrent() => Items[Selected];
-    
+    public Item? GetCurrent() {
+        return Items.Count == 0 ? null : Items[Selected];
+    }
+
     public void Equip() {
         var worldItem = GetCurrent();
+        if (worldItem == null) return;
         OnEquip?.Invoke(worldItem);
-        if (worldItem is WeaponMeleeItem melee) WeaponMeleeEquipped = melee;
-        if (worldItem is WeaponRangeItem range) WeaponRangeEquipped = range;
+        if (worldItem is WeaponMeleeItem melee) {
+            WeaponRangeEquipped = null;
+            WeaponMeleeEquipped = melee;
+        }
+        if (worldItem is WeaponRangeItem range) {
+            WeaponRangeEquipped = range;
+            WeaponMeleeEquipped = null;
+        }
+        Log();
     }
 
     public void Equip(int pos) {
@@ -38,8 +49,26 @@ public class Inventory {
 
     public void RemoveElement(Item item) {
         Items.Remove(item);
+        Log();
     }
 
-    public void NextItem() => Selected = (Selected + 1).Mod(Items.Count);
-    public void PrevItem() => Selected = (Selected - 1).Mod(Items.Count);
+    // print the items, showing which on is equipped comparing it withe WeaponMeleeEquipped
+    public void Log() {
+        for (var i = 0; i < Items.Count; i++) {
+            var item = Items[i];
+            var itemName = i == Selected ? $"[{item.Name}]" : $" {item.Name}";
+            Console.WriteLine(itemName+(item == WeaponMeleeEquipped || item == WeaponRangeEquipped ? " E" : ""));
+        }
+    }
+
+
+    public void NextItem() {
+        if (Items.Count == 0) return;
+        Selected = (Selected + 1).Mod(Items.Count);
+    }
+
+    public void PrevItem() {
+        if (Items.Count == 0) return;
+        Selected = (Selected - 1).Mod(Items.Count);
+    }
 }
