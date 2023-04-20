090a8e8ddebc265526c9734040cf30a012f3f999
Alberto Vilches <194074+avilches@users.noreply.github.com>
2023-04-20 22:32:06 +0200

    AwaitFrameExtensions.cs: added OnComplete

--- Betauer.Core/src/Signal/AwaitFrameExtensions.cs
+++ Betauer.Core/src/Signal/AwaitFrameExtensions.cs
@@ -5,22 +5,20 @@ using Godot;
 namespace Betauer.Core.Signal; 
 
 public static partial class AwaitExtensions {
-        
-    public static SignalAwaiter AwaitPhysicsFrame(this Node node) =>
-        node.GetTree().ToSignal(node.GetTree(), "physics_frame");
 
-    public static SignalAwaiter AwaitProcessFrame(this Node node) =>
-        node.GetTree().ToSignal(node.GetTree(), "process_frame");
+    public static SignalAwaiter AwaitPhysicsFrame(this Node node, Action? onComplete = null) =>
+        node.GetTree().AwaitPhysicsFrame(onComplete);
 
-    public static async Task AwaitPhysicsFrame(this Node node, int times) {
-        times = Math.Max(1, times);
+    public static SignalAwaiter AwaitProcessFrame(this Node node, Action? onComplete = null) =>
+        node.GetTree().AwaitProcessFrame(onComplete);
+
+    public static async Task AwaitPhysicsFrame(this Node node, int times, Action? onComplete = null) {
         var tree = node.GetTree();
-        while (times-- > 0) await tree.ToSignal(tree, "physics_frame");
+        while (times-- > 0) await tree.AwaitPhysicsFrame(onComplete);
     }
 
-    public static async Task AwaitProcessFrame(this Node node, int times) {
-        times = Math.Max(1, times);
+    public static async Task AwaitProcessFrame(this Node node, int times, Action? onComplete = null) {
         var tree = node.GetTree();
-        while (times-- > 0) await tree.ToSignal(tree, "process_frame");
+        while (times-- > 0) await tree.AwaitProcessFrame(onComplete);
     }
 }
