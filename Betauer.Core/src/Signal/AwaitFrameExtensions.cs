fa2f9b421f417711c69f561b46d1f194ab624b8a
Alberto Vilches <194074+avilches@users.noreply.github.com>
2023-05-24 12:23:55 +0200

    AwaitFrameExtensions.cs: renamed AwaitPhysicsFrame() to AwaitPhysicsFrameTimes() to avoid collision between method names

--- Betauer.Core/src/Signal/AwaitFrameExtensions.cs
+++ Betauer.Core/src/Signal/AwaitFrameExtensions.cs
@@ -12,13 +12,17 @@ public static partial class AwaitExtensions {
     public static SignalAwaiter AwaitProcessFrame(this Node node, Action? onComplete = null) =>
         node.GetTree().AwaitProcessFrame(onComplete);
 
-    public static async Task AwaitPhysicsFrame(this Node node, int times, Action? onComplete = null) {
-        var tree = node.GetTree();
+    public static Task AwaitPhysicsFrameTimes(this Node node, int times, Action? onComplete = null) =>
+        AwaitPhysicsFrameTimes(node.GetTree(), times, onComplete);
+
+    public static Task AwaitProcessFrameTimes(this Node node, int times, Action? onComplete = null) =>
+        AwaitProcessFrameTimes(node.GetTree(), times, onComplete);
+
+    public static async Task AwaitPhysicsFrameTimes(this SceneTree tree, int times, Action? onComplete = null) {
         while (times-- > 0) await tree.AwaitPhysicsFrame(onComplete);
     }
 
-    public static async Task AwaitProcessFrame(this Node node, int times, Action? onComplete = null) {
-        var tree = node.GetTree();
+    public static async Task AwaitProcessFrameTimes(this SceneTree tree, int times, Action? onComplete = null) {
         while (times-- > 0) await tree.AwaitProcessFrame(onComplete);
     }
 }
