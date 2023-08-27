5da7d24646f37e1c5f77c0dea6c3cb9d8e8037e8
Alberto Vilches <194074+avilches@users.noreply.github.com>
2023-08-27 13:05:53 +0200

    VariablePoissonSampler2DExtensions.cs: GenerateRandom() and GenerateFromNoise()

---
+++ Betauer.Core/src/PoissonDiskSampling/VariablePoissonSampler2DExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Betauer.Core.Image;
+using Godot;
+
+namespace Betauer.Core.PoissonDiskSampling; 
+
+public static class VariablePoissonSampler2DExtensions {
+
+    public static List<Vector2> GenerateRandom(this VariablePoissonSampler2D sampler, float minRadius, float maxRadius, Random? random = null) {
+        return sampler.Generate((x, y) => random.Range(minRadius, maxRadius), minRadius, maxRadius, random);
+    }
+    
+    public static List<Vector2> GenerateFromNoise(this VariablePoissonSampler2D sampler, FastNoise fastNoise, float minRadius, float maxRadius, Random? random = null) {
+        return sampler.Generate((x, y) => {
+            var noise = fastNoise.GetNoise((int)x, (int)y);
+            return Mathf.Lerp(minRadius, maxRadius, noise);
+        }, minRadius, maxRadius, random);
+    }
+    
+    
+}
