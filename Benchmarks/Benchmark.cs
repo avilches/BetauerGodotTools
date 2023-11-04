db38ec3c718bc09e79d167a4fcbdef07a7fadbe8
Alberto Vilches <194074+avilches@users.noreply.github.com>
2023-11-04 18:43:22 +0100

    Benchmark

--- Benchmarks/Benchmark.cs
+++ Benchmarks/Benchmark.cs
@@ -1,68 +1,56 @@
+using System.Runtime.CompilerServices;
 using BenchmarkDotNet.Attributes;
 using Betauer.Core;
-using Betauer.Core.PoissonDiskSampling;
-using Godot;
 
 namespace Benchmarks;
 
 [MemoryDiagnoser(true)]
 public class Benchmark {
-    // private VariablePoissonSampler2DOriginal original;
-    private VariablePoissonSampler2D better;
-    private Random r1 = new Random(0);
-    private Random r2 = new Random(0);
-    private Random r3 = new Random(0);
-
-    private const float minRadius = 6f;
-    private const float maxRadius = 20f;
-    
-    
-    public Benchmark() {
-        better = new VariablePoissonSampler2D( 512, 512);
-        a = 1;
-        b = 2;
-    }
-
-    //
-    // [Benchmark]
-    // public List<Vector2> PoissonOriginal() {
-    //     return original.Generate((x, y) => Range(r2, minRadius, maxRadius), minRadius, maxRadius);
-    // }
-    //
-    // [Benchmark]
-    // public List<Vector2> PoissonBetter() {
-    //     return better.Generate((x, y) => Range(r3, minRadius, maxRadius), minRadius, maxRadius, r3);
-    // }
-    //
-    // public static float Range(Random random, float start, float endExcluded) {
-    //     if (start > endExcluded) (start, endExcluded) = (endExcluded, start);
-    //     var limit = endExcluded - start;
-    //     var rn = random.NextDouble() * limit;
-    //     return (float)rn + start;
-    // }
 
     public enum MyEnum {
-        A,
-        B,
-        C,
+        A = 12, B
+    }
+
+    public class MyClass<T> where T : Enum {
+
+        public T GetFromLambda(int x) {
+            return x.ToEnum<T>();
+        }
+
+        public T GetFromCast(int x) {
+            return (T) (object) x;
+        }
+
+        public T GetFromParse(int x) {
+            return (T)Enum.Parse(typeof(T), x.ToString(), true);
+        }
+
+        public T GetFromUnsafe(int x) {
+            return Unsafe.As<int, T>(ref x);
+        }
+
+    }
+    
+    public MyClass<MyEnum> myClass = new MyClass<MyEnum>();
+    
+    [Benchmark]
+    public void GetFromLambda() {
+        myClass.GetFromLambda(12);
+    }
+    
+    [Benchmark]
+    public void GetFromCast() {
+        myClass.GetFromCast(12);
+    }
+    
+    [Benchmark]
+    public void GetFromParse() {
+        myClass.GetFromParse(12);
     }
 
     [Benchmark]
-    public void CompareEquals() {
-        var x = MyEnum.A.Equals(MyEnum.B);
-    }
-
-    [Benchmark]
-    public void CompareToInt() {
-        var x = MyEnum.A.ToInt() == MyEnum.B.ToInt();
-    }
-
-    public int a = 1;
-    public int b = 1;
-
-    [Benchmark]
-    public void CompareInts() {
-        var x = a == b;
+    public void GetFromUnsafe() {
+        myClass.GetFromUnsafe(12);
     }
 
 }
---
+++ Benchmarks/BenchmarkEnumCompare.cs
@@ -0,0 +1,41 @@
+using BenchmarkDotNet.Attributes;
+using Betauer.Core;
+using Betauer.Core.PoissonDiskSampling;
+using Godot;
+
+namespace Benchmarks;
+
+[MemoryDiagnoser(true)]
+public class BenchmarkEnumCompare {
+    public enum MyEnum {
+        A,
+        B,
+        C,
+    }
+
+    public int a = 1;
+    public int b = 1;
+    public MyEnum A = MyEnum.A;
+    public MyEnum B = MyEnum.B;
+
+    [Benchmark]
+    public void CompareEquals() {
+        var x = MyEnum.A.Equals(MyEnum.B);
+    }
+
+    [Benchmark]
+    public void CompareToInt() {
+        var x = MyEnum.A.ToInt() == MyEnum.B.ToInt();
+    }
+
+    [Benchmark]
+    public void CompareInts() {
+        var x = a == b;
+    }
+
+    [Benchmark]
+    public void CompareEnums() {
+        var x = A == B;
+    }
+
+}
