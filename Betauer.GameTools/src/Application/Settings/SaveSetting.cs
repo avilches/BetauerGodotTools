de607b78db05b98853c2bf5eb01418ebf5acd7ce
Alberto Vilches <194074+avilches@users.noreply.github.com>
2024-01-24 22:47:53 +0100

    SaveSetting: make the default "Settings" section configurable with a static var

--- Betauer.GameTools/src/Application/Settings/SaveSetting.cs
+++ Betauer.GameTools/src/Application/Settings/SaveSetting.cs
@@ -9,10 +9,23 @@ public static class Setting {
 
 public abstract class SaveSetting {
     public SettingsContainer? SettingsContainer { get; internal set; }
-    public string SaveAs { get; protected set; }
+    public string SaveAs { get; }
     public bool AutoSave { get; set; }
     public event Action? OnValueChanged;
-    
+
+    /// <summary>
+    /// If you want a different default section, just change this value at the very beginning of your game like this:
+    /// SaveSetting.DefaultSection = "Configuration";
+    /// </summary>
+    public static string DefaultSection = "Settings";
+
+    protected SaveSetting(string saveAs) {
+        if (!saveAs.Contains('/')) {
+            saveAs = $"{DefaultSection}/{saveAs}";
+        }
+        SaveAs = saveAs;
+    }
+
     protected void TriggerValueChanged() {
         OnValueChanged?.Invoke();
     }
@@ -32,11 +45,7 @@ public class SaveSetting<[MustBeVariant] T> : SaveSetting {
                                                        ?? throw new NullReferenceException(
                                                            $"{nameof(SettingsContainer)} is not initialized. Add it to a SettingsContainer with Add()");
 
-    public SaveSetting(string saveAs, T defaultValue, bool autoSave) {
-        if (!saveAs.Contains('/')) {
-            saveAs = $"Settings/{saveAs}";
-        }
-        SaveAs = saveAs;
+    public SaveSetting(string saveAs, T defaultValue, bool autoSave) : base(saveAs) {
         DefaultValue = defaultValue;
         AutoSave = autoSave;
     }
