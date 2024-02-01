0458d85b9f250ad26e3ede05c9ab32904a49a25a
Alberto Vilches <194074+avilches@users.noreply.github.com>
2024-02-01 14:34:41 +0100

    Rts: added Color to Biome

--- DemoGame/Game/RTS/World/Biome.cs
+++ DemoGame/Game/RTS/World/Biome.cs
@@ -1,8 +1,10 @@
 using System;
+using Godot;
 
 namespace Veronenger.Game.RTS.World;
 
 public class Biome<T> where T : Enum {
+    public Color Color;
     public char Char { get; set; }
     public T Type { get; set; }
     public (int Min, int Max) Temperature { get; set; }
