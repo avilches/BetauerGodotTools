5ff25c98785d4e28e9c9e1e9b7e6f4f431d380cb
avilches <github@betauer.aleeas.com>
2024-11-04 14:17:48 -0500

    Betauer.Core: added Draw.Path with methods LineOneTurn, LineTwoTurns and LineRandom

--- Betauer.Core/src/Image/Draw.Line.cs
+++ Betauer.Core/src/Image/Draw.Line.cs
@@ -16,30 +16,48 @@ public static partial class Draw {
             Line1Width(x0, y0, x1, y1, onPixel);
             return;
         }
-        int dx = Math.Abs(x1-x0), sx = x0 < x1 ? 1 : -1; 
-        int dy = Math.Abs(y1-y0), sy = y0 < y1 ? 1 : -1; 
-        int err = dx-dy, e2, x2, y2;                          /* error value e_xy */
-        var ed = dx+dy == 0 ? 1f : Mathf.Sqrt((float)dx*dx+(float)dy*dy);
+        if (x0 == x1) {
+            // Vertical line
+            if (y0 > y1) (y0, y1) = (y1, y0);
+            for (var w = -width / 2; w <= width / 2; w++) {
+                for (var i = y0; i <= y1; i++) {
+                    onPixel(x0 + w, i);
+                }
+            }
+        } else if (y0 == y1) {
+            // Horizontal line
+            if (x0 > x1) (x0, x1) = (x1, x0);
+            for (var w = -width / 2; w <= width / 2; w++) {
+                for (var i = x0; i <= x1; i++) {
+                    onPixel(i, y0 + w);
+                }
+            }
+        } else {
+            // Diagonal
+            int dx = Math.Abs(x1-x0), sx = x0 < x1 ? 1 : -1;
+            int dy = Math.Abs(y1-y0), sy = y0 < y1 ? 1 : -1;
+            int err = dx - dy, e2, x2, y2; /* error value e_xy */
+            var ed = dx+dy == 0 ? 1f : Mathf.Sqrt((float)dx*dx + (float)dy*dy);
 
-        for (width = (width + 1) / 2;;) { /* pixel loop */
-            onPixel(x0, y0);
-            e2 = err; x2 = x0;
-            if (2 * e2 >= -dx) { /* x step */
-                for (e2 += dy, y2 = y0; e2 < ed * width && (y1 != y2 || dx > dy); e2 += dx)
-                    onPixel(x0, y2 += sy);
-                if (x0 == x1) break;
-                e2 = err; err -= dy; x0 += sx; 
+            for (width = (width + 1) / 2;;) { /* pixel loop */
+                onPixel(x0, y0);
+                e2 = err; x2 = x0;
+                if (2 * e2 >= -dx) { /* x step */
+                    for (e2 += dy, y2 = y0; e2 < ed * width && (y1 != y2 || dx > dy); e2 += dx)
+                        onPixel(x0, y2 += sy);
+                    if (x0 == x1) break;
+                    e2 = err; err -= dy; x0 += sx; 
+                }
+                if (2 * e2 <= dy) { /* y step */
+                    for (e2 = dx - e2; e2 < ed * width && (x1 != x2 || dx < dy); e2 += dy)
+                        onPixel(x2 += sx, y0);
+                    if (y0 == y1) break;
+                    err += dx; y0 += sy;
+                }
             }
-            if (2 * e2 <= dy) { /* y step */
-                for (e2 = dx - e2; e2 < ed * width && (x1 != x2 || dx < dy); e2 += dy)
-                    onPixel(x2 += sx, y0);
-                if (y0 == y1) break;
-                err += dx; y0 += sy; 
-            }
-        }    
+        }
     }
 
-    // width 1
     private static void Line1Width(int x0, int y0, int x1, int y1, Action<int, int> onPixel) {
         if (x0 == x1 && y0 == y1) {
             // one point case
@@ -84,12 +102,12 @@ public static partial class Draw {
             } /* e_xy+e_y < 0 */
         }
     }
-    
+
     // http://members.chello.at/~easyfilter/bresenham.html
     public static void LineAntialiasing(Vector2I start, Vector2I end, int width, Action<int, int, float> onPixel) {
         LineAntialiasing(start.X, start.Y, end.X, end.Y, width, onPixel);
     }
-    
+
     public static void LineAntialiasing(int x0, int y0, int x1, int y1, int width, Action<int, int, float> onPixel) {
         if (width == 0) return;
         if (width == 1) {
@@ -97,7 +115,7 @@ public static partial class Draw {
             return;
         }
         int dx = Math.Abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
-        int dy = Math.Abs(y1 - y0), sy = y0 < y1 ? 1 : -1; 
+        int dy = Math.Abs(y1 - y0), sy = y0 < y1 ? 1 : -1;
         int err = dx-dy, e2, x2, y2;                          /* error value e_xy */
         var ed = dx + dy == 0 ? 1f : Mathf.Sqrt((float)dx * dx + (float)dy * dy);
 
@@ -116,13 +134,13 @@ public static partial class Draw {
                 if (y0 == y1) break;
                 err += dx; y0 += sy; 
             }
-        }    
+        }
     }
-    
+
     // aliasing with width 1
     private static void LineAntialiasing1Width(int x0, int y0, int x1, int y1, Action<int, int, float> onPixel) {
         int dx = Math.Abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
-        int dy = Math.Abs(y1 - y0), sy = y0 < y1 ? 1 : -1; 
+        int dy = Math.Abs(y1 - y0), sy = y0 < y1 ? 1 : -1;
         int err = dx-dy, e2, x2;                       /* error value e_xy */
         var ed = dx + dy == 0 ? 1f : (float)Math.Sqrt((float)dx * dx + (float)dy * dy);
 
@@ -192,5 +210,5 @@ public static partial class Draw {
         c -= a; c -= b; c ^= (b >> 15);
         return c % 999999;
     }
-
+    
 }
---
+++ Betauer.Core/src/Image/Draw.Path.cs
@@ -0,0 +1,112 @@
+using System;
+using Godot;
+
+namespace Betauer.Core.Image;
+
+public static partial class Draw {
+    public static Random Random = new Random();
+
+    public static void LineOneTurn(int x0, int y0, int x1, int y1, Action<int, int> onPixel, bool firstHorizontal = true) {
+        // Move in one direction until aligned, then move in the other direction
+        if (firstHorizontal) {
+            /*
+             ...........
+             .########..
+             ........#..
+             ........#..
+             ...........
+            */
+            // Move horizontally first until aligned, then move vertically to reach the target
+            for (; x0 != x1; x0 += (x1 > x0) ? 1 : -1) onPixel(x0, y0);
+            for (; y0 != y1; y0 += (y1 > y0) ? 1 : -1) onPixel(x0, y0);
+        } else {
+            /*
+             ...........
+             .#.........
+             .#.........
+             .########..
+             ...........
+            */
+            // Move vertically first until aligned, then move horizontally to reach the target
+            for (; y0 != y1; y0 += (y1 > y0) ? 1 : -1) onPixel(x0, y0);
+            for (; x0 != x1; x0 += (x1 > x0) ? 1 : -1) onPixel(x0, y0);
+        }
+        onPixel(x0, y0);
+    }
+
+    /*
+     ...........
+     ...........
+     .#.........
+     .#.........
+     .########..
+     ........#..
+     ........#..
+     ........#..
+     ...........
+     ...........
+    */
+    public static void LineTwoTurns(int x0, int y0, int x1, int y1, Action<int, int> onPixel, bool firstHorizontal = true, float firstTurn = 0.5f, float secondTurn = 0.5f) {
+        var midX = Math.Clamp(Mathf.RoundToInt((x0 + x1) * firstTurn), x0, x1);
+        var midY = Math.Clamp(Mathf.RoundToInt((y0 + y1) * secondTurn), y0, y1);
+        if (firstHorizontal) {
+            for (; x0 != midX; x0 += (midX > x0) ? 1 : -1) onPixel(x0, y0);
+            for (; y0 != midY; y0 += (midY > y0) ? 1 : -1) onPixel(x0, y0);
+            for (; x0 != x1; x0 += (x1 > x0) ? 1 : -1) onPixel(x0, y0);
+            for (; y0 != y1; y0 += (y1 > y0) ? 1 : -1) onPixel(x0, y0);
+        } else {
+            for (; y0 != midY; y0 += (midY > y0) ? 1 : -1) onPixel(x0, y0);
+            for (; x0 != midX; x0 += (midX > x0) ? 1 : -1) onPixel(x0, y0);
+            for (; y0 != y1; y0 += (y1 > y0) ? 1 : -1) onPixel(x0, y0);
+            for (; x0 != x1; x0 += (x1 > x0) ? 1 : -1) onPixel(x0, y0);
+        }
+        onPixel(x0, y0);
+    }
+
+    public static void LineRandom(int x0, int y0, int x1, int y1, Action<int, int> onPixel, int maxStraightLength = 3, Random? random = null) {
+        /*
+        #.........
+        #.........
+        #####.....
+        ....#.....
+        ....###...
+        ......#...
+        ......#...
+        ......###.
+        ........##
+        .........#
+        */
+        random ??= Random;
+        if (maxStraightLength <= 0) maxStraightLength = int.MaxValue;
+        var dx = Math.Abs(x1 - x0);
+        var dy = Math.Abs(y1 - y0);
+        var totalDistance = dx + dy;
+        var offset = 0;
+        while (x0 != x1 || y0 != y1) {
+            onPixel(x0, y0);
+            var horizontalProbability = (float)dx / totalDistance;
+            if (random.NextDouble() < horizontalProbability) {
+                offset++;
+                if (offset > maxStraightLength) {
+                    offset = 0;
+                    if (y0 < y1) y0++;
+                    else if (y0 > y1) y0--;
+                } else {
+                    if (x0 < x1) x0++;
+                    else if (x0 > x1) x0--;
+                }
+            } else {
+                offset--;
+                if (offset < -maxStraightLength) {
+                    offset = 0;
+                    if (x0 < x1) x0++;
+                    else if (x0 > x1) x0--;
+                } else {
+                    if (y0 < y1) y0++;
+                    else if (y0 > y1) y0--;
+                }
+            }
+        }
+        onPixel(x0, y0);
+    }
+}
