c837bb4441a6d9f979c009ba2b39fb46c63ea248
avilches <github@betauer.aleeas.com>
2024-11-05 21:37:45 -0500

    Betauer.Core: SpatialGridExtensions, fix ExpandAll = added bounds and return the elements expanded

--- Betauer.Core/src/Collision/Spatial2D/SpatialGridExtensions.cs
+++ Betauer.Core/src/Collision/Spatial2D/SpatialGridExtensions.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Godot;
 
 namespace Betauer.Core.Collision.Spatial2D;
@@ -81,31 +82,77 @@ public static class SpatialGridExtensions {
     /// </summary>
     /// <param name="grid"></param>
     /// <param name="amount"></param>
-    public static void ExpandAll(this SpatialGrid grid, float amount) {
-        var pending = grid.FindShapes();
-        if (pending.Count < 2) return; // at least 2 shapes are needed to expand, so they can touch each other
+    /// <returns>number of expansions. 0 means it was not possible to expand even once</returns>
+    public static void ExpandAll(this SpatialGrid grid, float amount, Rect2I? bounds = null) {
+        List<Shape> pending = grid.FindShapes<Circle>().Cast<Shape>().ToList();
+        pending.AddRange(grid.FindShapes<Rectangle>());
         while (pending.Count > 0) {
-            // remove all the pending circles that can't be expanded
-            pending.RemoveAll((shape) => {
-                switch (shape) {
-                    case Circle circle:
-                        if (circle.TryResize(circle.Radius + amount)) {
-                            return false; // circle was resized successfully, don't delete it
-                        }
-                        return true; // circle can't be resized, delete it
-                    case Rectangle rectangle: {
-                        if (rectangle.TryResize(rectangle.Width + amount, rectangle.Height) &&
-                            rectangle.TryResize(rectangle.Width, rectangle.Height + amount) &&
-                            rectangle.TryUpdate(rectangle.X - amount, rectangle.Y, rectangle.Width + amount, rectangle.Height) &&
-                            rectangle.TryUpdate(rectangle.X, rectangle.Y - amount, rectangle.Width, rectangle.Height + amount)) {
-                            return false; // shape was resized successfully, don't delete it 
-                        }
-                        return true; // rectangle failed at some step when resized/moved, delete it
-                    }
-                    default:
-                        return true; // unknown shape (point), can't be expanded, delete it
-                }
-            });
+            ExpandAll(grid, pending, amount, bounds);
         }
+    }
+
+    /// <summary>
+    /// Try to expand the shapes in the grid by the given amount without touching each other.
+    /// It returns only the shapes expanded, so it can be called multiple times until no more shapes can be expanded.
+    /// </summary>
+    /// <param name="grid"></param>
+    /// <param name="pending"></param>
+    /// <param name="amount"></param>
+    /// <returns>number of expansions. 0 means it was not possible to expand even once</returns>
+    public static void ExpandAll(this SpatialGrid grid, List<Shape> pending, float amount, Rect2I? bounds = null) {
+        if (pending.Count < 2) {
+            pending.Clear();
+            return; // at least 2 shapes are needed to expand, so they can touch each other
+        }
+        var expanded = 0;
+        const bool KEEP_EXPANDING_THIS_SHAPE = false;
+        const bool STOP_EXPANDING_THIS_SHAPE = true;
+        // remove all the pending circles that can't be expanded
+        pending.RemoveAll((shape) => {
+            switch (shape) {
+                case Circle circle:
+                    if (circle.TryResize(circle.Radius + amount)) {
+                        if (bounds.HasValue && ReachBounds(bounds.Value, circle)) {
+                            circle.Update(circle.X, circle.Y, circle.Radius - amount);
+                            // shape was resized successfully, but it reached the bounds, restore previous size and delete it
+                            return STOP_EXPANDING_THIS_SHAPE;
+                        }
+                        expanded++;
+                        return KEEP_EXPANDING_THIS_SHAPE; // shape was resized successfully, don't delete it 
+                    }
+                    return STOP_EXPANDING_THIS_SHAPE; // circle can't be resized, delete it
+                case Rectangle rectangle: {
+                    var copy = rectangle.ToRect2();
+                    if (rectangle.TryResize(rectangle.Width + amount, rectangle.Height) &&
+                        rectangle.TryResize(rectangle.Width, rectangle.Height + amount) &&
+                        rectangle.TryUpdate(rectangle.X - amount, rectangle.Y, rectangle.Width + amount, rectangle.Height) &&
+                        rectangle.TryUpdate(rectangle.X, rectangle.Y - amount, rectangle.Width, rectangle.Height + amount)) {
+                        if (bounds.HasValue && ReachBounds(bounds.Value, rectangle)) {
+                            rectangle.Update(copy.Position.X, copy.Position.Y, copy.Size.X, copy.Size.Y);
+                            // shape was resized successfully, but it reached the bounds, restore previous size and delete it
+                            return STOP_EXPANDING_THIS_SHAPE;
+                        }
+                        expanded++;
+                        return KEEP_EXPANDING_THIS_SHAPE; // shape was resized successfully, don't delete it 
+                    }
+                    
+                    // rectangle failed at some step when resized/moved, delete it
+                    // before deleting it, restore the previous size
+                    if (bounds.HasValue && ReachBounds(bounds.Value, rectangle)) {
+                        rectangle.Update(copy.Position.X, copy.Position.Y, copy.Size.X, copy.Size.Y);
+                    }
+                    return STOP_EXPANDING_THIS_SHAPE; 
+                }
+                default:
+                    return STOP_EXPANDING_THIS_SHAPE; // unknown shape (point), can't be expanded, delete it
+            }
+        });
+    }
+
+    private static bool ReachBounds(Rect2I bounds, Shape rectangle) {
+        return rectangle.MaxX > bounds.Size.X ||
+               rectangle.MaxY > bounds.Size.Y ||
+               rectangle.MinX < bounds.Size.X ||
+               rectangle.MinY < bounds.Size.Y;
     }
 }
