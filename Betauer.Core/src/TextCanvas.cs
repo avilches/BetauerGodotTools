75788dbd500659d24e16bd370c87385517b551ba
avilches <github@betauer.aleeas.com>
2024-11-26 23:48:22 -0500

    Betauer.Core: TextCanvas now allow any size

--- Betauer.Core/src/TextCanvas.cs
+++ Betauer.Core/src/TextCanvas.cs
@@ -1,15 +1,16 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Betauer.Core;
 
 public class TextCanvas {
-    private readonly string[] _lines;
+    private readonly List<string> _lines;
     private char _lineSeparator;
     private string? _text = null;
 
-    public int Width { get; }
-    public int Height { get; }
+    public int Width { get; private set; } = 0;
+    public int Height { get; private set; } = 0;
 
     public char LineSeparator {
         get => _lineSeparator;
@@ -19,8 +20,8 @@ public class TextCanvas {
         }
     }
 
-    public TextCanvas(int width, int height, char lineSeparator = '\n') :
-        this(Enumerable.Repeat(new string(' ', width), height).ToArray(), lineSeparator) {
+    public TextCanvas(char lineSeparator = '\n') : this([""], lineSeparator) {
+        
     }
 
     public TextCanvas(string initialContent, char lineSeparator = '\n') :
@@ -34,13 +35,17 @@ public class TextCanvas {
     }
 
     public TextCanvas(string[] lines, char lineSeparator = '\n') {
-        if (lines.Length == 0) throw new ArgumentException("Initial content cannot be empty");
-        _lines = lines;
-        Width = _lines[0].Length;
-        Height = _lines.Length;
+        _lines = [..lines];
         LineSeparator = lineSeparator;
+        var width = _lines.Max(line => line.Length);
+        EnsureSize(width, _lines.Count);
+    }
 
-        if (_lines.Any(line => line.Length != Width)) throw new ArgumentException("All lines must be the same length");
+    private void Update() {
+        for (var i = 0; i < _lines.Count; i++) {
+            if (_lines[i].Length < Width) _lines[i] = _lines[i].PadRight(Width);
+        }
+        Height = _lines.Count;
     }
 
     public string GetText() => _text ??= string.Join(LineSeparator, _lines);
@@ -62,20 +67,30 @@ public class TextCanvas {
     }
 
     private void _Write(int column, int row, string text) {
-        if (string.IsNullOrEmpty(text)) return;
-        if (row < 0 || row >= _lines.Length) throw new ArgumentOutOfRangeException(nameof(row), $"Row is out of canvas bounds: {row}");
-        if (column < 0 || column >= Width) throw new ArgumentOutOfRangeException(nameof(column), $"Column is out of canvas bounds: {column}");
-        if (column + text.Length > Width) throw new ArgumentException($"Text is too wide: {text.Length}. From the position {column}, the space is only {Width - column} chars long.");
+        if (column < 0) throw new ArgumentException("Column is out of canvas bounds: " + row);
+        if (row < 0) throw new ArgumentException("Row is out of canvas bounds: " + row);
+        text ??= "";
+        EnsureSize(column + text.Length, row + 1);
         var line = _lines[row];
         _lines[row] = line.Substring(0, column) + text + line.Substring(column + text.Length);
         _text = null;
     }
 
+    private void EnsureSize(int width, int height) {
+        if (width > Width) {
+            for (var i = 0; i < _lines.Count; i++) {
+                _lines[i] = _lines[i].PadRight(width);
+            }
+        }
+        Width = width;
+        while (height > _lines.Count) {
+            _lines.Add(new string(' ', Width));
+        }
+        Height = _lines.Count;
+    }
+
     public void Fill(int x, int y, int width, int height, char fillChar) {
-        if (x < 0 || x >= Width || y < 0 || y >= _lines.Length)
-            throw new ArgumentException("Region starts outside of the canvas");
-        if (x + width > Width || y + height > _lines.Length)
-            throw new ArgumentException("Region doesn't fit in the canvas.");
+        EnsureSize(x + width, y + height);
 
         for (var i = y; i < y + height; i++) {
             var line = _lines[i].ToCharArray();
@@ -90,7 +105,7 @@ public class TextCanvas {
     public void WriteCentered(int centerX, int centerY, string text) {
         if (string.IsNullOrEmpty(text)) return;
         var textStartX = centerX - text.Length / 2;
-        if (textStartX < 0 || centerY < 0 || centerY >= _lines.Length)
+        if (textStartX < 0 || centerY < 0 || centerY >= _lines.Count)
             throw new ArgumentException("Centered text doesn't fit in the canvas.");
         Write(textStartX, centerY, text);
     }
